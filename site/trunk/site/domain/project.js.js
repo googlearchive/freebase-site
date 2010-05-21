26d96c21bb0a46def20ac470babd23da308460c0
kconragan@google.com <kconragan@google.com@ff54e9e8-a3ca-d32d-4494-0c755e69c161>
2010-05-21 21:30:15 +0000

    Migrating my prototype work from published_project into domain

--- site/trunk/site/domain/project.js.js
+++ site/trunk/site/domain/project.js.js
@@ -7,28 +7,21 @@ $(document).ready(function(){
     // $("#collections").masonry({animate: true });
 
     $(".collection-img > a").hover(function(){
-        $(this).find('img').animate({left:'-604px'},{duration:4500, easing: 'linear'});},function() {
+        $(this).find('img').animate({left: '0'},{duration:1000}).animate({left:'-604px'},{duration:4500, easing: 'linear'});},function() {
         $(this).find('img').stop(true, true);
         $(this).find('img').animate({left:'0px'},{duration:500, easing: 'swing'});
     });
 
     $(".collection-show-topics").click(function(){
-    
-    
         var collection_id = $(this).attr("title");
-        var $topics = $(this).parent().siblings(".collection-topics");
-        var $menu = $(this).closest(".collection-info");
-        var $container = $(this).closest(".collection");
+        var $collection = $(this).closest(".collection");
+        var $topics = $collection.find(".collection-topics");
         
         if ($topics.is(":hidden")) {
-            $menu.animate({top: '50px'}, 300);
-            $topics.slideDown(300).fadeIn(200);
-            $topics.load("http://published_project.site.freebase.dev.acre.z:8115/collection-topics?id=" + collection_id);
+            show_topic_panel($collection, $topics, collection_id);
         }
-
         else {
-            $menu.animate({top: '153px'}, 300);
-            $topics.slideUp(300).fadeOut(200);
+            hide_topic_panel($collection);
         }
     });
     
@@ -36,6 +29,25 @@ $(document).ready(function(){
         $(this).addClass('collection-active');
     }, function() {
         $(this).removeClass('collection-active');
+        hide_topic_panel($(this));
     });
+    
+
+    function show_topic_panel(collection, topics, collection_id) {
+        $(collection).find(".collection-info").animate({top: '50px'}, 300);
+        $(topics).slideDown(300).fadeIn(200);
+        $(topics).load("http://published_project.site.freebase.dev.acre.z:8115/collection-topics?id=" + collection_id);    
+    }
+    
+    function hide_topic_panel(collection) {
+        
+        var $menu = $(collection).find(".collection-info");
+        var $topics = $(collection).find(".collection-topics");
+        
+        if($topics.is(":visible")) {
+            $menu.animate({top: '162px'}, 300);
+            $topics.slideUp(300).fadeOut(200);        
+        }
+    }
     
 });

64e60db903fb6e05ff91b13217f783cf37113bea
kconragan@google.com <kconragan@google.com@ff54e9e8-a3ca-d32d-4494-0c755e69c161>
2010-05-22 00:40:11 +0000

    Improved interaction for collection rollover and more responsive Ajax handling.

--- site/trunk/domain/project.js.js
+++ site/trunk/domain/project.js.js
@@ -1,11 +1,12 @@
 $(document).ready(function(){
     
     // Only load chiclets when needed
-    //$("img").lazyload();
+    $("img").lazyload();
   
     // Setup jQuery masonry to handle reflowing of chiclets
-    // $("#collections").masonry({animate: true });
+    $("#collections").masonry({animate: true });
 
+    // We animate the chiclet on hover after 1 second, and return to 0 on mouseout
     $(".collection-img > a").hover(function(){
         $(this).find('img').animate({left: '0'},{duration:1000}).animate({left:'-604px'},{duration:4500, easing: 'linear'});},function() {
         $(this).find('img').stop(true, true);
@@ -13,41 +14,51 @@ $(document).ready(function(){
     });
 
     $(".collection-show-topics").click(function(){
+    
+        // Basic UI state management for collection topics
         var collection_id = $(this).attr("title");
         var $collection = $(this).closest(".collection");
         var $topics = $collection.find(".collection-topics");
         
         if ($topics.is(":hidden")) {
+            $(this).addClass("expanded");
             show_topic_panel($collection, $topics, collection_id);
         }
         else {
+            $(this).removeClass("expanded");
             hide_topic_panel($collection);
         }
     });
     
+    // Add active class to hovered collection &
+    // Remove active class and hide topics on mouseout
     $(".collection").hover(function() {
         $(this).addClass('collection-active');
     }, function() {
         $(this).removeClass('collection-active');
+        $(this).find(".collection-show-topics").removeClass("expanded");
         hide_topic_panel($(this));
     });
     
-
+    // Function for showing topics panel
     function show_topic_panel(collection, topics, collection_id) {
         $(collection).find(".collection-info").animate({top: '50px'}, 300);
-        $(topics).slideDown(300).fadeIn(200);
-        $(topics).load("http://published_project.site.freebase.dev.acre.z:8115/collection-topics?id=" + collection_id);    
+        $(topics).slideDown(300).fadeIn(200).addClass("loading");
+        $.ajax({
+            url: "http://domain.site.freebase.dev.acre.z:8115/collection-topics?id=" + collection_id,
+            success: function(data) {
+                $(topics).removeClass("loading").html(data);
+            }
+        });
     }
     
+    // Function for hiding topics panel
     function hide_topic_panel(collection) {
-        
         var $menu = $(collection).find(".collection-info");
         var $topics = $(collection).find(".collection-topics");
-        
         if($topics.is(":visible")) {
             $menu.animate({top: '162px'}, 300);
             $topics.slideUp(300).fadeOut(200);        
         }
     }
-    
 });

ad9fc3095c3b444150b7f61ae71b7d640b229e51
masouras@google.com <masouras@google.com@ff54e9e8-a3ca-d32d-4494-0c755e69c161>
2010-05-26 17:56:26 +0000

    multiple fixes

--- site/trunk/scripts/acrepush.py
+++ site/trunk/scripts/acrepush.py
@@ -4,6 +4,8 @@ import os
 import hashlib
 import urllib2
 import re
+import hashlib
+import pdb
 
 try:
     import json
@@ -79,92 +81,219 @@ class OnDiskAcreApp(object):
             raise Exception("need to supply an id if .metadata is not present")
 
         def handle_file(f):
-            script = {'id':null, 'name':null, 'handler':null,
+            script = {'id':null, 'name':null, 'acre_handler':null,
                       'content_type':null, 'contents':null, 'extension':null}
             fn, ext = f.rsplit('.', 1)
             script['id'] = metadata['id'] + '/' + quotekey(fn)
             script['extension'] = ext
             script['name'] = quotekey(fn)
+            script['unquoted_filename'] = fn
             script['contents'] = file(os.path.join(directory, f))
-            script['blob_id'] = hashlib.sha256(script['contents'].read()).hexdigest()
+            script['SHA256'] = hashlib.sha256(script['contents'].read()).hexdigest()
             ct, handler = extmap.type_for_extension(ext)
-            script['handler'] = handler
+            script['acre_handler'] = handler
             script['content_type'] = ct
 
             return script
 
         metadata['files'] = {}
+        metadata['ignored_files'] = []
 
         # Skip . .. .xxxx xxx.sh and directories
+
         for f in os.listdir(directory):
             basename, extension = os.path.splitext(f)
-            if extension in ['.sh'] or f.startswith('.') or os.path.isdir(os.path.join(directory, f)):
-                print >> sys.stderr, "Info: Skipping "+f
+
+            if extension in ['.sh'] or f.startswith('.') or os.path.isdir(os.path.join(directory, f)) or \
+                   f[-1] == '~' or basename[len(basename)-4:] in ['.mql', '.sjs', '.mjt']:
+                metadata['ignored_files'].append(f)
                 continue
 
             d = handle_file(f)
+
+            if metadata['files'].get(d['name']):
+                print >> sys.stderr, 'WARNING: file %s will override contents of %s' % (f, d['name'])
 
             metadata['files'][d['name']] = d
 
         return metadata
 
 class AcrePush(object):
-    def __init__(self, user, pw, acrehost=DEFAULT_ACRE_SERVICE_URL):
+    def __init__(self, acrehost=DEFAULT_ACRE_SERVICE_URL):
 
         u = urllib2.urlopen(acrehost+'/acre/status').read()
         me_server = u.split('\n')[2].split(':')[1].strip()
         self.fb = HTTPMetawebSession(me_server, cookiefile='', 
                                      acre_service_url=acrehost)
 
-        self.fb.login(user, pw)
-
-    def push(self, directory, version, id=None, patch=False):
-        oda = OnDiskAcreApp(directory, id)
+    def get_credentials(self, user=None, pw=None):
 
         try:
-            self.fb.get_app(oda.metadata['id'])
+            if not user:
+                user = raw_input("Username: ")
+
+            import getpass
+            if not pw:
+                pw = getpass.getpass()
+        except KeyboardInterrupt:
+            print >> sys.stderr, "\nPush aborted."
+            return (None, None)
+
+        return (user, pw)
+
+    def print_app_diff(self, delete_files, push_files, ignored_files):
+        
+        for filename in ignored_files:
+            print >> sys.stderr, "?\t%s" % filename
+        for filename,d in delete_files.iteritems():
+            if filename not in push_files.keys():
+                print >> sys.stderr, "R\t%s\t(%s)" % (d.get('unquoted_filename'), d.get('reason', ''))
+                
+        for filename,d in push_files.iteritems():
+            if filename in delete_files.keys() or d.get('reason', '') == 'changed content':
+                print >> sys.stderr, "M\t%s\t(%s)" % (d.get('unquoted_filename'), d.get('reason', ''))
+            else:
+                print >> sys.stderr, "A\t%s\t(%s)" % (d.get('unquoted_filename'), d.get('reason', ''))
+                    
+
+
+    def get_app_diff(self, graph_app, ondisk_app):
+        graph_files, local_files = graph_app['files'], ondisk_app.metadata['files']
+
+        #the files we need to delete either because they are not in the local directory
+        #or because their content-type or handler has changed
+        delete_files = {}
+        #the files we need to push because they have changed since the last push
+        push_files = {}
+
+        #helper for comparing handlers and content type
+        def different_handler_and_content_type(graph_stat, local_stat):
+            for check in ['content_type', 'acre_handler']:
+                if graph_stat[check] != local_stat[check]:
+                    return "%s updated" % check
+
+            return False
+            
+        #first iterate through the graph files and delete any file that does not exist locally
+        for filename in graph_files.keys():
+            if not filename in local_files.keys():
+                delete_files[filename] = { 'reason' : 'not in local directory' }
+
+
+        #now iterate through the local files
+        for filename, local_stat in local_files.iteritems():
+
+            #if it's a new file, just push it
+            if not graph_files.get(filename):
+                local_stat['reason'] = 'new file'
+                push_files[filename] = local_stat
+                continue
+
+            graph_stat = graph_files[filename]
+
+            #if the handler or content type have changed, we need to delete the old file and re-push
+            different = different_handler_and_content_type(graph_stat, local_stat)
+            if different:
+                delete_files[filename] = { 'reason' : different }
+                local_stat['reason'] = different
+                push_files[filename] = local_stat
+                break
+
+            #now check if the file contents have changed
+            if graph_stat['SHA256'] != local_stat['SHA256']:
+                local_stat['reason'] = 'changed content'
+                push_files[filename] = local_stat
+
+
+        self.print_app_diff(delete_files, push_files, ondisk_app.metadata['ignored_files'])
+
+        return (delete_files, push_files)
+
+    def push(self, directory, version, id=None, dry=False, user=None, pw=None):
+        ondisk_app = OnDiskAcreApp(directory, id)
+        graph_app = None
+
+        try:
+            graph_app = self.fb.get_app(ondisk_app.metadata['id'])
         except MetawebError:
             if patch:
                 usage("Cannot patch a non-existant app")
-            self.fb.create_app(oda.metadata['id'])
+            self.fb.create_app(ondisk_app.metadata['id'])
 
-        if not patch:
-            self.fb.delete_app_all_files(oda.metadata['id'])
+        delete_files, push_files = {}, ondisk_app.metadata['files']
 
-        for key, val in oda.metadata['files'].iteritems():
+        if graph_app:
+            (delete_files, push_files) = self.get_app_diff(graph_app, ondisk_app)
+
+        if dry:
+            print "Not making any changes (dry run)"
+            return
+
+
+        ###### dry run until this point ##########
+
+        if version or len(delete_files.keys()) or len(push_files.keys()):
+            (user, pw) = self.get_credentials(user, pw)
+            self.fb.login(user, pw)
+            
+        if version:
+            self.fb.create_app_version(ondisk_app.metadata['id'], version)
+            print >> sys.stderr, 'Updated version %s' % version
+
+        if not (len(delete_files.keys()) or len(push_files.keys())):
+            print "No files affected."
+            return
+
+        files_changed = set()
+        if not (user and pw):
+            return
+
+        for filename,val in delete_files.iteritems():
+            print ".",            
+            self.fb.delete_app_file(ondisk_app.metadata['id'], unquotekey(filename))
+            files_changed.add(filename)
+
+        for filename, val in push_files.iteritems():
+            print ".",            
             val['contents'].seek(0);
-            if val['handler'] == 'binary':
+            files_changed.add(filename)
+            if val['acre_handler'] == 'binary':
                 self.fb.save_binary_file(val['id'], val['contents'],
                                          val['content_type'])
             else:
                 self.fb.save_text_file(val['id'], val['contents'].read(),
-                                       val['handler'], val['content_type'])
+                                       val['acre_handler'], val['content_type'])
 
 
-        self.fb.create_app_version(oda.metadata['id'], version)
 
 
+        print "\nPush succesfull, %s files affected" % len(files_changed)
+        
 
 def usage(msg=None):
     if msg:
         print >> sys.stderr, "%s: %s" %(sys.argv[0], msg)
         print >> sys.stderr, ""
 
-    print >> sys.stderr, "%s [-i id] [-h acrehost] [-u username] [-p password] [-P] directory version" % sys.argv[0]
+    print >> sys.stderr, "%s [-i id] [-h acrehost] [-u username] [-p password] [-d] directory [version]" % sys.argv[0]
     sys.exit(1)
 
 if __name__ == '__main__':
     import getopt
 
     try:
-        args, remains = getopt.getopt(sys.argv[1:], "i:h:u:p:P")
+        args, remains = getopt.getopt(sys.argv[1:], "i:h:u:p:d")
     except getopt.GetoptError, e:
         usage(e)
 
-    if len(remains) != 2:
-        usage("must supply directory and version")
+    if not len(remains):
+        usage("must supply app directory ")
 
-    id, host, user, pw, patch = None, DEFAULT_ACRE_SERVICE_URL, None, None, False
+    directory, version = remains[0], None
+    if len(remains) > 1:
+        version = remains[1]
+
+    id, host, user, pw, dry = None, DEFAULT_ACRE_SERVICE_URL, None, None, False
     for a in args:
         if a[0] == '-i':
             id = a[1]
@@ -174,15 +303,8 @@ if __name__ == '__main__':
             user = a[1]
         elif a[0] == '-p':
             pw = a[1]
-        elif a[0] == '-P':
-            patch = True
-
-    if not user:
-        user = raw_input("Username: ")
-
-    import getpass
-    if not pw:
-        pw = getpass.getpass()
+        elif a[0] == '-d':
+            dry = True
 
     mhost = SHORT_GRAPH_MAP.get(host);
     if not host.startswith("http://") and not mhost:
@@ -191,5 +313,5 @@ if __name__ == '__main__':
     if mhost:
         host = mhost
         
-    ap = AcrePush(user, pw, host);
-    ap.push(remains[0], remains[1], id, patch)
+    ap = AcrePush(host);
+    ap.push(directory, version, id, dry, user, pw)

27c2bcbc6af5a1e5a7424c5ed2de96179f2a2c3f
culbertson@google.com <culbertson@google.com@ff54e9e8-a3ca-d32d-4494-0c755e69c161>
2010-06-30 01:59:55 +0000

    Add spinner and pointer like the explore page

--- site/trunk/homepage/homepage.js.js
+++ site/trunk/homepage/homepage.js.js
@@ -12,12 +12,30 @@ $.tools.tabs.addEffect("load_pane", function(i, done) {
     $pane.fadeIn(done);
   }
   
+  function update_pointer() {
+      // First, figure out the selected tab and it's position
+      var tab_position = $tab.position();
+      
+      // Update pointer text and position accordingly
+      var pointer_text = $tab.text();
+      $("#pointer-text").fadeOut(function() {$("#pointer-text").html(pointer_text);});
+      $("#pointer").fadeIn().animate({'top': tab_position.top + 'px' },
+                                     function() {$("#pointer-text").fadeIn();});
+  }
+  
+  function switch_pane() {
+    $tab.parent().removeClass("processing");
+    crossfade();
+    update_pointer();
+  }
+  
   // only load the pane once
   if ($pane.is(":empty") || $.trim($pane.text()).length == 0) {
     // load it with a page specified in the tab's href attribute
-    $pane.load($tab.attr("href"), crossfade);
+    $tab.parent().addClass("processing");
+    $pane.load($tab.attr("href"), switch_pane);
   } else {
-    crossfade();
+    switch_pane();
   };
 });
 

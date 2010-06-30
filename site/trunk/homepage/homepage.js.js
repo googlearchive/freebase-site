9918be20a6e4a56bb0623b7750cf78e4d066852e
culbertson@google.com <culbertson@google.com@ff54e9e8-a3ca-d32d-4494-0c755e69c161>
2010-06-30 01:25:28 +0000

    Make the tabs work with fades

--- site/trunk/homepage/homepage.js.js
+++ site/trunk/homepage/homepage.js.js
@@ -2,11 +2,30 @@
 
 fb.homepage = {};
 
+$.tools.tabs.addEffect("load_pane", function(i, done) {
+  var $panes = this.getPanes();
+  var $pane = $panes.eq(i);
+  var $tab = this.getTabs().eq(i);
+  
+  function crossfade() {
+    $panes.fadeOut("fast");
+    $pane.fadeIn(done);
+  }
+  
+  // only load the pane once
+  if ($pane.is(":empty") || $.trim($pane.text()).length == 0) {
+    // load it with a page specified in the tab's href attribute
+    $pane.load($tab.attr("href"), crossfade);
+  } else {
+    crossfade();
+  };
+});
+
 fb.homepage.init = function() {
   
   $("#domain-explorer-tabs").tabs("#explorer-panes > div", {
-    effect: "ajax",
-    initialIndex: 0
+    initialIndex: 0,
+    effect: "load_pane"
   });
 
 };

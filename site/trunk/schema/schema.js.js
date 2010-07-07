d5dd5bf2532d267aae1e76da0919d93cf12f692b
kconragan@google.com <kconragan@google.com@ff54e9e8-a3ca-d32d-4494-0c755e69c161>
2010-07-07 22:49:15 +0000

    Search now works for schema explorer for Domain, Type,and Property.

--- site/trunk/schema/schema.js.js
+++ site/trunk/schema/schema.js.js
@@ -1,95 +1,143 @@
 $(document).ready(function(){
 
+    // Setup schema search tabset
     $("#schema-search > .section-tabset").tabs("#schema-search > .search-box");
 
 
+    var MQL_FILTERS = {
+        domain : [{ "key": [{"namespace" : "/" }] }],
+        type : [{ "/type/type/domain": [{ "key" : [{ "namespace" : "/" }] }], "a:/type/type/domain": { "id": "/freebase", "optional" : "forbidden" } }],
+        property : [{ "/type/property/schema": { "type": "/type/type", "domain": [{ "key" : [{ "namespace" : "/" }] }], "a:domain" : { "id" : "/freebase", "optional" : "forbidden" } } }]
+    }
+ 
+    /* 
+        DOMAIN SUGGEST
+    */
+    var $domain_input = $("#domain-search-input");
+    var $domain_form = $domain_input.closest("form");
+    
+    var domain_suggest_options = { "type" : "/type/domain" };
+    
+    if ($("#domain-search-toggle-commons").is(":checked")) {
+        domain_suggest_options.mql_filter = MQL_FILTERS.domain;
+    }
+
+    $domain_input.suggest(domain_suggest_options)
+    .bind("fb-select", function(e, data){
+        $domain_form.find("input[type=text]").val(data.id)
+        $domain_form.submit();
+    })
+    .focus(function() {
+        this.select();
+    });
+    
+    /*
+        TYPE SUGGEST
+    */
+    var $type_input = $("#type-search-input");
+    var $type_form = $type_input.closest("form");
+
+    var type_suggest_options = { "type" : "/type/type" };
+
+    if ($("#type-search-toggle-commons").is(":checked")) {
+        type_suggest_options.mql_filter = MQL_FILTERS.type;
+    }
+    
+    $type_input.suggest(type_suggest_options)
+   .bind("fb-select", function(e, data){
+        $type_form.find("input[type=text]").val(data.id);
+        $type_form.submit();
+    })
+    .focus(function() {
+        this.select();
+    });
+
+    /*
+        PROPERTY SUGGEST
+    */
+    var $property_input = $("#property-search-input");
+    var $property_form = $property_input.closest("form");
+    
+    var property_suggest_options = { "type" : "/type/property" };
+
+    if ($("#property-search-toggle-commons").is(":checked")) {
+        property_suggest_options.mql_filter = MQL_FILTERS.property;
+    }
+    
+    $property_input.suggest(property_suggest_options)
+   .bind("fb-select", function(e, data){
+        $property_form.find("input[type=text]").val(data.id);
+        $property_form.submit();
+    })
+    .focus(function() {
+        this.select();
+    });
+
+    /*
+        USER SUGGEST
+    */
 /*
-    $("#domain").suggest({
-    "type": "/type/domain"
-    }).bind("fb-select", function(e,data) { 
-    var t = "#domain";
-    $(t).val(data.id);
-    $(t + "_form").submit();
-    }).focus(function() {
-    this.select();
-    });
-    
-    $("#user").suggest({
-    "type": "/type/user"
-    }).bind("fb-select", function(e,data) { 
-    var t = "#user";
-    $(t).val(data.id);
-    $(t + "_form").submit();
-    }).focus(function() {
-    this.select();
-    });
-    
-    $("#type").suggest({
-    "type" : "/type/type",
-    "mql_filter" : [{
-      "type" : "/type/type",
-      "domain": [{
-        "key": [{
-          "namespace": "/"
-        }]
-      }],
-      "a:domain": {
-        "id":       "/freebase",
-        "optional": "forbidden"
-      }
-    }]
-    }).bind("fb-select", function(e,data) { 
-    var t = "#type";
-    $(t).val(data.id);
-    $(t + "_form").submit();
-    }).focus(function() {
-    this.select();
-    });
-    
-    $("#property").suggest({
-    "type" : "/type/property",
-    "mql_filter" : [{
-      "type": "/type/property",
-      "schema": {
-        "type": "/type/type",
-        "domain": [{
-          "key": [{
-            "namespace": "/"
-          }]
-        }],
-        "a:domain": {
-          "id": "/freebase",
-          "optional": "forbidden"
-        }
-      }
-    }]
-    }).bind("fb-select", function(e,data) { 
-    var t = "#property";
-    $(t).val(data.id);
-    $(t + "_form").submit();
-    }).focus(function() {
-    this.select();
-    });
-    
-    $("#all_type").suggest({
-    "type": "/type/type"
-    }).bind("fb-select", function(e,data) {
-    var t = "#type";
-    $(t).val(data.id);
-    $(t + "_form").submit();
-    }).focus(function() {
-    this.select();
-    });
-    
-    $("#all_property").suggest({
-    "type": "/type/property"
-    }).bind("fb-select", function(e,data) {
-    var t = "#property";
-    $(t).val(data.id);
-    $(t + "_form").submit();
-    }).focus(function() {
-    this.select();
-    });
+    var $user_input = $("#user-search-input");
+    var $user_form = $user_input.closest("form");
 */
+    
+
+    // On click for radio buttons, we have to update mql_filter params and reinitialize suggest
+    $(".search-toggle").click(function(e){
+        
+        // We grab the radio buttons closest form
+        // and compare it's ID to decide which mql_filter
+        // we need to update
+
+        $el = $(this);
+        $parent = $(this).closest("form");
+
+        // Split ID to compare string
+        el_id = $el.attr("id").split("-");
+        
+        // Domain
+        if ($parent.attr("id") === $domain_form.attr("id")) {
+        
+            if (el_id[el_id.length-1] === "commons") {            
+                domain_suggest_options.mql_filter = MQL_FILTERS.domain;
+            }
+            else {
+                delete domain_suggest_options.mql_filter;
+            }
+
+            $domain_input.suggest(domain_suggest_options);        
+        }
+        
+        // Type
+        if ($parent.attr("id") === $type_form.attr("id")) {
+        
+            if (el_id[el_id.length-1] === "commons") {            
+                type_suggest_options.mql_filter = MQL_FILTERS.type;
+            }
+            else {
+                delete type_suggest_options.mql_filter;
+            }
+
+            $type_input.suggest(type_suggest_options);
+        }
+        
+        // Property
+        if ($parent.attr("id") === $property_form.attr("id")) {
+
+            if (el_id[el_id.length-1] === "commons") {            
+                property_suggest_options.mql_filter = MQL_FILTERS.property;
+            }
+            else {
+                delete property_suggest_options.mql_filter;
+            }
+            
+            $property_input.suggest(property_suggest_options);
+
+        }
+
+    });
+
+
+
 
 });

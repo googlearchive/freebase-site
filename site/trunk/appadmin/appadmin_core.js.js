1290a1ab927701c3b337effbfcc28e163da90376
masouras@google.com <masouras@google.com@ff54e9e8-a3ca-d32d-4494-0c755e69c161>
2010-08-10 23:27:29 +0000

    can release apps and switch environments

--- site/trunk/appadmin/appadmin_core.js.js
+++ site/trunk/appadmin/appadmin_core.js.js
@@ -1,1 +1,57 @@
-var foo = 5;
+$(".summary").each(function(index) {  
+    load_app_summary($(this).attr('app_id'));
+});
+
+$("#release-all").click(function(e) { 
+    $(".release").each(function(index) { $(this).click(); });
+    return false;
+});
+
+
+function load_app_summary(appid) { 
+    $("#summary-" + appid).load(
+	$("#summary-" + appid).attr('app_url'),
+	[],
+	function(text, status, req) { 
+	    bind_app_buttons(appid);
+	}	
+    );
+}
+
+
+function bind_app_buttons(appid) { 
+
+    $(".release").each(function(index) { 
+	$(this).click(function(e) { 
+
+	    html_id = $(this).attr('app_html_id');
+	    
+	    $.ajax({
+		'url' : $(this).attr('href'),
+		'data' : {'appid' : $(this).attr('app_id'), 'version' : $(this).attr('app_version') },
+		'type' : 'POST',
+		'dataType' : 'json',
+		'success' : function(data, textStatus, req) { 
+		    $("#message").html("App " + data.result.appid + " version " + data.result.release + " has been released.");
+		    load_app_summary(html_id);
+		},
+		'beforeSend' : function(req) { 
+		    req.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+		}
+	    
+	    });
+	    
+	    return false;
+	});
+    });
+}
+
+/*
+  $(".expand").each(function(index) { 
+  $(this).click(function(e) { 
+  $("#details-" + $(this).attr('app_id')).show();
+  $("#details-" + $(this).attr('app_id')).load($(this).attr('app_url'));
+  return false;
+  })})
+*/
+

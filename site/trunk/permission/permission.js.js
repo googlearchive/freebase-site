2487ab370b2a99639ad324ee61b68a1e8d452dfc
daepark@google.com <daepark@google.com@ff54e9e8-a3ca-d32d-4494-0c755e69c161>
2010-08-10 02:33:28 +0000

    bootstrap permission lookup on fb.user.signedin event

--- site/trunk/permission/permission.js.js
+++ site/trunk/permission/permission.js.js
@@ -1,34 +1,43 @@
 
 (function($, fb) {
-   if (!fb.user) {
-     // if no user, don't need to waste our time with the permission query
-     return;
-   }
-   if (typeof acre === "undefined" || typeof acre.c === "undefined") {
-     // all templates that go through /freebase/site/template/freebase.mjt will declare an "acre" var.
-     return;
-   }
-   var c = acre.c;
-   if (!(c && c.id)) {
-     // c.id is the primary node in question
-     return;
-   }
 
-   //console.log("permission.js", c.id, fb.user.id);
+   var p = fb.permission = {
+     init: function() {
+       if (!fb.user) {
+         // if no user, don't need to waste our time with the permission query
+         return;
+       }
+       if (typeof acre === "undefined" || typeof acre.c === "undefined") {
+         // all templates that go through /freebase/site/template/freebase.mjt will declare an "acre" var.
+         return;
+       }
+       var c = acre.c;
+       if (!(c && c.id)) {
+         // c.id is the primary node in question
+         return;
+       }
 
-   var perm = fb.permission = {
-     jsonp: function(data) {
-       perm.has_permission = data.result === true;
-       console.log("has_permission", perm.has_permission);
-       $(window).trigger("fb.permission.has_permission", perm.has_permission);
+       //console.log("permission.js", c.id, fb.user.id);
+
+       var perm = fb.permission = {
+         jsonp: function(data) {
+           perm.has_permission = data.result === true;
+           //console.log("has_permission", perm.has_permission);
+           $(window).trigger("fb.permission.has_permission", perm.has_permission);
+         }
+       };
+
+       // does fb.user have permission on c.id?
+       $.ajax({
+         url: acre.request.app_url + "/permission/service/has_permission",
+         data: {id:c.id, user_id:fb.user.id},
+         dataType: "jsonp",
+         jsonpCallback: "window.freebase.permission.jsonp"
+       });
      }
    };
-   
-   // does fb.user have permission on c.id?
-   $.ajax({
-     url: acre.request.app_url + "/permission/service/has_permission",
-     data: {id:c.id, user_id:fb.user.id},
-     dataType: "jsonp",
-     jsonpCallback: "window.freebase.permission.jsonp"
-   });
+
+   // only check permission if user is signedin
+   $(window).bind("fb.user.signedin", p.init);
+
 })(jQuery, window.freebase);

c1b5edd70d7d04e87e2bb147da25939c201618f3
wdm@google.com <wdm@google.com@ff54e9e8-a3ca-d32d-4494-0c755e69c161>
2010-10-28 18:31:35 +0000

    Added notes: how to sync codemirror

--- site/trunk/third_party/build.sh
+++ site/trunk/third_party/build.sh
@@ -2,6 +2,18 @@
 
 # This is a hack to work-around the lack of directory support in the acre deploy system
 
+# To setup the git repo
+
+#  cd codemirror
+#  git init .
+#  git remote add origin http://github.com/marijnh/CodeMirror.git
+
+# To update:
+#  git pull origin master
+#  svn status
+#  ../build.sh
+#  (and svn commit if all is well)
+
 # Normalize the PARENT directory
 cd `dirname $0`/..
 

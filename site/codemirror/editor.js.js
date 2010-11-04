97e9e463c3a08701ee2a431a440c1d793a821dc6
wdm@google.com <wdm@google.com@ff54e9e8-a3ca-d32d-4494-0c755e69c161>
2010-11-04 18:20:48 +0000

    Removed Google copyright from non-Google files

--- site/codemirror/editor.js.js
+++ site/codemirror/editor.js.js
@@ -1,34 +1,3 @@
-/*
- * Copyright 2010, Google Inc.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are
- * met:
- *
- *     * Redistributions of source code must retain the above copyright
- * notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above
- * copyright notice, this list of conditions and the following disclaimer
- * in the documentation and/or other materials provided with the
- * distribution.
- *     * Neither the name of Google Inc. nor the names of its
- * contributors may be used to endorse or promote products derived from
- * this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 /* The Editor object manages the content of the editable frame. It
  * catches events, colours nodes, and indents lines. This file also
  * holds some functions for transforming arbitrary DOM structures into
@@ -382,7 +351,7 @@ var Editor = (function(){
           }
           node = next;
           offset = 0;
-        }        
+        }
       }
     },
 
@@ -995,7 +964,7 @@ var Editor = (function(){
         else if (Editor.Parser.firstIndentation)
           newIndent = Editor.Parser.firstIndentation(nextChars, curIndent, direction);
       }
-      
+
       var indentDiff = newIndent - curIndent;
 
       // If there is too much, this is just a matter of shrinking a span.
@@ -1451,7 +1420,7 @@ var Editor = (function(){
       }
       // Create a part corresponding to a given token.
       function tokenPart(token){
-        var part = makePartSpan(token.value);     
+        var part = makePartSpan(token.value);
         part.className = token.style;
         return part;
       }
@@ -1529,7 +1498,7 @@ var Editor = (function(){
               select.snapshotMove(old.firstChild, part && (part.firstChild || part), 0);
             }
           }
-          
+
           return part;
         }
       };

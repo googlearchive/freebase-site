d0729b147ab451c0dc2c48869dbe72a71630c0fe
jasondouglas@google.com <jasondouglas@google.com@ff54e9e8-a3ca-d32d-4494-0c755e69c161>
2010-12-07 19:57:04 +0000

    ACRE-1820
    View/Preview button should use versioned URL

--- site/appeditor/acre_app.js.js
+++ site/appeditor/acre_app.js.js
@@ -272,7 +272,7 @@ var AcreApp;
     };
     
     AcreApp.prototype.get_base_url = function() {
-        return "http:" + this.get_path() + "." + this.get_acre_host();
+        return "http:" + this.get_versioned_path() + "." + this.get_acre_host();
     };
         
     AcreApp.prototype.get_view_url = function() {

131df5ef969c932da64b304a8ac8b64ccb29897a
bneutra@google.com <bneutra@google.com@ff54e9e8-a3ca-d32d-4494-0c755e69c161>
2010-12-15 01:19:48 +0000

    include elapsed time when test fails

--- scripts/nosetester/test.py
+++ scripts/nosetester/test.py
@@ -7,14 +7,17 @@
 from nosetester import *
 t = Controller()
 from nose.plugins import skip
+import time
 
 # this class facilitates generation of nose tests with proper names
 class TstResult:
-    def __init__(self, k, results):
+    def __init__(self, k, results, elapsed):
         self.description = k
         self.results = results
+        self.elapsed = elapsed
     def __call__(self, k):
         r = self.results[k]
+        print 'time for %s: %2.3f seconds' % (k.split(':')[0], self.elapsed)
         if r[0] is False:
             print r[1]
             assert 'an acre test failed' is True
@@ -26,9 +29,11 @@ class TstResult:
 def test_apps():
     t.freebase_login()
     for u in t.test_urls:
+        starttime = time.time()
         # actually run the test, hit the url
         res = t.run_acre_tst(u)
+        elapsed = time.time() - starttime
         # evaluate json results for all tests in url
         for k in res:
-            yield TstResult(k, res), k
+            yield TstResult(k, res, elapsed), k
 

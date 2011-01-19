bcc3eb26e17f7a0a59ac82d838e81f81e8368c4c
masouras@google.com <masouras@google.com@ff54e9e8-a3ca-d32d-4494-0c755e69c161>
2011-01-19 20:27:33 +0000

    Modified manifest resource files: codemirror/codemirror-frame.javascript.mf.css codemirror/codemirror-frame.javascript.mf.js codemirror/codemirror-frame.mf.css codemirror/codemirror-frame.mf.js codemirror/codemirror.mf.js cuecard/cuecard.mf.css cuecard/cuecard.mf.js editor-bundle.mf.css editor-bundle.mf.js error/error.mf.css flot/flot.all.mf.js flot/flot.core.mf.js jqueryui/jquery.ui.core.mf.js mjt/mjt-template.mf.js mjt/mjt.mf.js template/freebase.mf.css template/freebase.mf.js template/freebase.ui.mf.css template/jquery.placeholder.test.mf.js test/qunit.mf.css test/qunit.mf.js

--- tags/www/lib/2j/codemirror/parsedummy.js
+++ tags/www/lib/2j/codemirror/parsedummy.js
@@ -1,32 +1,14 @@
-var DummyParser = Editor.Parser = (function() {
-  function tokenizeDummy(source) {
-    while (!source.endOfLine()) source.next();
-    return "text";
-  }
-  function parseDummy(source) {
-    function indentTo(n) {return function() {return n;}}
-    source = tokenizer(source, tokenizeDummy);
-    var space = 0;
-
-    var iter = {
-      next: function() {
-        var tok = source.next();
-        if (tok.type == "whitespace") {
-          if (tok.value == "\n") tok.indentation = indentTo(space);
-          else space = tok.value.length;
-        }
-        return tok;
-      },
-      copy: function() {
-        var _space = space;
-        return function(_source) {
-          space = _space;
-          source = tokenizer(_source, tokenizeDummy);
-          return iter;
-        };
-      }
-    };
-    return iter;
-  }
-  return {make: parseDummy};
-})();
+[
+  "codemirror/util.js", 
+  "codemirror/stringstream.js", 
+  "codemirror/select.js", 
+  "codemirror/undo.js", 
+  "codemirror/editor.js", 
+  "codemirror/tokenize.js",
+  "codemirror/parsexml.js", 
+  "codemirror/parsecss.js", 
+  "codemirror/tokenizejavascript.js", 
+  "codemirror/parsejavascript.js", 
+  "codemirror/parsehtmlmixed.js", 
+  "codemirror/parsedummy.js"
+]
--- tags/www/lib/2j/template/freebase.mf.js
+++ tags/www/lib/2j/template/freebase.mf.js
@@ -1,10 +1,1 @@
-[
-  "template/json2.js",
-  "template/jquery.cookie.js",
-  "template/jquery.localstore.js",
-  "template/jquery.placeholder.js",
-  "template/jquery.metadata.js",
-  "jqueryui/jquery.ui.core.mf.js",
-  "template/freebase.js",
-  "permission/permission.js"
-]
+["template/json2.js", "template/jquery.cookie.js", "template/jquery.localstore.js", "template/jquery.placeholder.js", "template/jquery.metadata.js", "jqueryui/_jquery.ui.core.mf.js", "template/freebase.js", "permission/permission.js"]

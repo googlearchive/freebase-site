d15414f64e647cde42b4d8391aa9b355f3602352
bneutra@google.com <bneutra@google.com@ff54e9e8-a3ca-d32d-4494-0c755e69c161>
2011-02-11 23:10:27 +0000

    sample cfg file

---
+++ scripts/buildbot/bbcfg.py
@@ -0,0 +1,7 @@
+# put this in the same dir as the master.cfg
+cfg = {
+"fromaddr" : "an_email_address",
+"authkey" : "an auth key for our test db",
+"extraRecipients" : ["an_email_address"],
+"dburl" : "http://our_test_db.foo/api/submit"
+}

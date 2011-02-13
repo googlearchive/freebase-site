c901d24230ceaf55f8ac6d6f5508c34b9de330cc
jasondouglas@google.com <jasondouglas@google.com@ff54e9e8-a3ca-d32d-4494-0c755e69c161>
2011-02-13 20:04:20 +0000

    New queryeditor layout

--- trunk/www/queryeditor/index.js
+++ trunk/www/queryeditor/index.js
@@ -1,240 +1,246 @@
 /*
- * Copyright 2010, Google Inc.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are
- * met:
- *
- *     * Redistributions of source code must retain the above copyright
- * notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above
- * copyright notice, this list of conditions and the following disclaimer
- * in the documentation and/or other materials provided with the
- * distribution.
- *     * Neither the name of Google Inc. nor the names of its
- * contributors may be used to endorse or promote products derived from
- * this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
+* Copyright 2010, Google Inc.
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are
+* met:
+*
+*     * Redistributions of source code must retain the above copyright
+* notice, this list of conditions and the following disclaimer.
+*     * Redistributions in binary form must reproduce the above
+* copyright notice, this list of conditions and the following disclaimer
+* in the documentation and/or other materials provided with the
+* distribution.
+*     * Neither the name of Google Inc. nor the names of its
+* contributors may be used to endorse or promote products derived from
+* this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+* "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+* LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+* A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+* OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+* SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+* LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+* DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+* THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
 
-CueCard.helper = fb.acre.request.app_url + "/cuecard/";
+CueCard.helper = fb.ajax.lib + "/cuecard/";
 CueCard.freebaseServiceUrl = fb.acre.freebase.service_url + "/";
-CueCard.urlPrefix = CueCard.apiProxy.base = fb.acre.ajax_base;
+CueCard.urlPrefix = CueCard.apiProxy.base = fb.ajax.lib + "/cuecard/";
 
 var c = {};
 var queryEditorOptions;
+var heights;
 
 function onLoad() {
+  var params = CueCard.parseURLParameters();
 
-    if ($.cookie("cc_cp") == "0") {
-      $("#the-control-pane")[0].style.display = "none";
-    }
+  var outputPaneOptions = {};
+  var controlPaneOptions = {
+    extended: 0 // Don't make this sticky: $.cookie("cc_cp_extended") == "1" ? 1 : 0
+  };
 
-    resizePanes();
-
-    var params = CueCard.parseURLParameters();
-
-    var outputPaneOptions = {};
-    var controlPaneOptions = {
-        extended: 0 // Don't make this sticky: $.cookie("cc_cp_extended") == "1" ? 1 : 0
-    };
-
-    queryEditorOptions = {
-        focusOnReady: true,
-        onUnauthorizedMqlWrite: function() {
-            if (window.confirm("Query editor needs to be authorized to write data on your behalf. Proceed to authorization?")) {
-                saveQueryInWindow();
-
-                var url = document.location.href;
-                var hash = url.indexOf("#");
-                if (hash > 0) {
-                    url = url.substr(0, hash);
-                } else {
-                    var question = url.indexOf("?");
-                    if (question > 0) {
-                        url = url.substr(0, question);
-                    }
-                }
-
-                var url2 = "/signin/login?mw_cookie_scope=domain&onsignin=" + encodeURIComponent(url);
-
-                document.location = url2;
-            }
-        },
-        emql: "emql" in params && params["emql"] == "1",
-        service: "service" in params ? params["service"] : null
-    };
-
-    if ("debug" in params) {
-        queryEditorOptions.debug = params.debug;
-    }
-
-    if ("q" in params || "query" in params) {
-        var s = "q" in params ? params.q : params.query;
-        try {
-            var o = JSON.parse(s);
-            if ("query" in o) {
-                s = JSON.stringify(o.query);
-
-                delete o.query;
-                controlPaneOptions["env"] = o;
-            }
-        } catch (e) {
-        }
-
-        queryEditorOptions.content = s;
-        queryEditorOptions.cleanUp = true;
-
-        if ("extended" in params) {
-            controlPaneOptions["extended"] = params["extended"] == "1" ? 1 : 0;
-        }
-        if ("as_of_time" in params) {
-            controlPaneOptions["as_of_time"] = params["as_of_time"];
-        }
-        if ("env" in params) {
-            try {
-                controlPaneOptions["env"] = JSON.parse(params["env"]);
-            } catch (e) {
-            }
-        }
-    } else {
-        try {
-            var o = JSON.parse(window.name);
-            if ($.trim(o.t).length > 0) {
-                queryEditorOptions.content = o.t;
-            }
-            controlPaneOptions.env = o.e;
-        } catch (e) {
-            if ("initialQuery" in window) {
-                queryEditorOptions.content = window.initialQuery;
-            }
-        }
-    }
-
-    c = CueCard.createComposition({
-        queryEditorElement: $('#the-query-editor')[0],
-        queryEditorOptions: queryEditorOptions,
-        outputPaneElement: $('#the-output-pane')[0],
-        outputPaneOptions: {},
-        controlPaneElement: $('#the-control-pane')[0],
-        controlPaneOptions: controlPaneOptions
-    });
-
-    $("#the-splitter").click(onToggleControlPane);
-
-    $(window).bind("beforeunload", function(evt) {
+  queryEditorOptions = {
+    focusOnReady: true,
+    onUnauthorizedMqlWrite: function() {
+      if (window.confirm("Query editor needs to be authorized to write data on your behalf. Proceed to authorization?")) {
         saveQueryInWindow();
-    });
 
-    if ("autorun" in params) {
-    	queryEditorOptions.onReady = function() {
+        var url = document.location.href;
+        var hash = url.indexOf("#");
+        if (hash > 0) {
+          url = url.substr(0, hash);
+        } else {
+          var question = url.indexOf("?");
+          if (question > 0) {
+            url = url.substr(0, question);
+          }
+        }
+
+        var url2 = "/signin/login?mw_cookie_scope=domain&onsignin=" + encodeURIComponent(url);
+
+        document.location = url2;
+      }
+    },
+    emql: "emql" in params && params["emql"] == "1",
+    service: "service" in params ? params["service"] : null
+  };
+
+  if ("debug" in params) {
+    queryEditorOptions.debug = params.debug;
+  }
+
+  if ("q" in params || "query" in params) {
+    var s = "q" in params ? params.q : params.query;
+    try {
+      var o = JSON.parse(s);
+      if ("query" in o) {
+        s = JSON.stringify(o.query);
+
+        delete o.query;
+        controlPaneOptions["env"] = o;
+      }
+    } catch (e) {
+    }
+
+    queryEditorOptions.content = s;
+    queryEditorOptions.cleanUp = true;
+
+    if ("extended" in params) {
+      controlPaneOptions["extended"] = params["extended"] == "1" ? 1 : 0;
+    }
+    
+    if ("as_of_time" in params) {
+      controlPaneOptions["as_of_time"] = params["as_of_time"];
+    }
+    if ("env" in params) {
+      try {
+        controlPaneOptions["env"] = JSON.parse(params["env"]);
+      } catch (e) {
+      }
+    }
+  } else {
+    try {
+      var o = JSON.parse(window.name);
+      if ($.trim(o.t).length > 0) {
+        queryEditorOptions.content = o.t;
+      }
+      controlPaneOptions.env = o.e;
+    } catch (e) {
+      if ("initialQuery" in window) {
+        queryEditorOptions.content = window.initialQuery;
+      }
+    }
+  }
+
+  c = CueCard.createComposition({
+    queryEditorElement: $('#the-query-editor')[0],
+    queryEditorOptions: queryEditorOptions,
+    outputPaneElement: $('#the-output-pane')[0],
+    outputPaneOptions: {},
+    controlPaneElement: $('#the-control-pane')[0],
+    controlPaneOptions: controlPaneOptions
+  });
+  
+  // patch in our changes to cuecard's default layout
+  $("#links").prependTo("#the-output-pane .cuecard-outputPane-tabs");
+  $("#qe-options").click(onToggleControlPane).prependTo(".cuecard-queryEditor-controls-bottom");
+  $("#the-control-pane").appendTo('#the-query-editor .cuecard-queryEditor');
+  
+  $(".module").collapse_module("#the-output-pane .cuecard-outputPane-content");
+  
+  heights = {
+    controlPane: 250,
+    header: $("#header").outerHeight(),
+    breadcrumb: $("#breadcrumb").outerHeight(),
+    footer: $("#footer").outerHeight(),
+    qeTitle: $("#qe-title").outerHeight(),
+    qeButtons: $(".cuecard-queryEditor-controls-bottom").outerHeight()
+  };
+  
+  resizePanes();
+  toggleControlPane(($.cookie("cc_cp") == "1"));
+
+  $(window).bind("beforeunload", function(evt) {
+    saveQueryInWindow();
+  });
+
+  if ("autorun" in params) {
+    queryEditorOptions.onReady = function() {
       c.queryEditor.run(false);
     };
   }
 }
 
 function resizePanes() {
-    var spacing = 10;
-    var controlPaneHeight = 250;
-    var splitterHeight = 12;
+  var innerHeight = $("body").outerHeight() - heights.header - heights.breadcrumb - heights.footer - 30; //padding
+  $(".cuecard-outputPane").height(innerHeight);
+  
+  var qeHeight = innerHeight - heights.qeTitle;
+  $(".cuecard-queryEditor").height(qeHeight);
 
-    var width = $("#content-main").width();
-    var halfWidth = Math.round((width - spacing) / 2);
-    var height = $(window).height() - $("#header").outerHeight(true)
-                 - $(".page-header").outerHeight(true) - $("#footer").outerHeight(true);
-
-    $("#qe-container").height(height - 30); // top margin on #footer
-
-    var innerHeight = height - 20; // bottom margin on query editor
-    if ($("#the-control-pane")[0].style.display == "block") {
-        var queryEditorHeight = innerHeight - splitterHeight - controlPaneHeight - 2 * spacing;
-        $("#the-query-editor").height(queryEditorHeight);
-        $("#the-splitter").height(splitterHeight).css("top", queryEditorHeight + spacing);
-        $("#the-control-pane").height(controlPaneHeight).css("top", queryEditorHeight + spacing + splitterHeight + spacing);
-        $("#the-control-pane").width(halfWidth);
-    } else {
-        var queryEditorHeight = innerHeight - splitterHeight - spacing;
-        $("#the-query-editor").height(queryEditorHeight);
-        $("#the-splitter").height(splitterHeight).css("top", queryEditorHeight + spacing);
-    }
-    $("#the-splitter").width(halfWidth);
-    $("#the-query-editor").width(halfWidth);
-    $("#the-output-pane").width(halfWidth).height(innerHeight);
+  var qeContentHeight =  qeHeight - heights.qeButtons - $("#the-control-pane").outerHeight();
+  $(".cuecard-queryEditor-content").height(qeContentHeight);
 };
 
 function onResize() {
-    resizePanes();
-
-    c.queryEditor.layout();
-    c.controlPane.layout();
-    c.outputPane.layout();
+  resizePanes();
+  //c.queryEditor.layout();
+  c.outputPane.layout();
+  c.controlPane.layout();
 }
 
 function onToggleControlPane() {
-    var cp = $("#the-control-pane")[0];
-    cp.style.display = cp.style.display == "block" ? "none" : "block";
-
-    onResize();
-
-    $.cookie("cc_cp", $("#the-control-pane")[0].style.display == "none" ? "0" : "1", { expires: 365 });
+  var on = ($.cookie("cc_cp") == "1");
+  toggleControlPane(!on);
+  $.cookie("cc_cp", (on ? "0" : "1"), { expires: 365 });
 }
 
+function toggleControlPane(show) {
+  if (show) {
+    $("#the-control-pane").height(heights.controlPane);
+    $(".cuecard-queryEditor-content").animate({height: "-=" + heights.controlPane}, function() {
+      $("#qe-options .edit-icon").text("Hide Options");
+      onResize();    
+    });
+  } else {
+    $(".cuecard-queryEditor-content").animate({height: "+=" + heights.controlPane}, function() {
+      $("#the-control-pane").height(0);
+      $("#qe-options .edit-icon").text("Show Options");
+      onResize();
+    });
+  }
+};
+
 function computePermanentLink(a) {
-    a.href = "?q=" + encodeURIComponent(c.queryEditor.content()) + getUrlFlags();
+  a.href = "?q=" + encodeURIComponent(c.queryEditor.content()) + getUrlFlags();
 }
 
 function computeCompactLink(a) {
-    a.href = "?q=" + encodeURIComponent(c.queryEditor.getUnresolvedQuery()) + getUrlFlags();
+  a.href = "?q=" + encodeURIComponent(c.queryEditor.getUnresolvedQuery()) + getUrlFlags();
 }
 
 function computeMqlReadLink(a) {
-    a.href = c.queryEditor.getMqlReadURL();
+  a.href = c.queryEditor.getMqlReadURL();
 }
 
 function getUrlFlags() {
-    var params = [];
+  var params = [];
 
-    var env = c.controlPane.getQueryEnvelope({}, true);
-    for (var n in env) {
-        params.push("env=" + JSON.stringify(env));
-        break;
-    }
+  var env = c.controlPane.getQueryEnvelope({}, true);
+  for (var n in env) {
+    params.push("env=" + JSON.stringify(env));
+    break;
+  }
 
-    if (queryEditorOptions.emql) {
-        params.push("emql=1");
-    }
-    if ("debug" in queryEditorOptions) {
-        params.push("debug=" + queryEditorOptions.debug);
-    }
-    if (queryEditorOptions.service != null) {
-        params.push("service=" + encodeURIComponent(queryEditorOptions.service));
-    }
+  if (queryEditorOptions.emql) {
+    params.push("emql=1");
+  }
+  if ("debug" in queryEditorOptions) {
+    params.push("debug=" + queryEditorOptions.debug);
+  }
+  if (queryEditorOptions.service != null) {
+    params.push("service=" + encodeURIComponent(queryEditorOptions.service));
+  }
 
-    return params.length == 0 ? "" : ("&" + params.join("&"));
+  return params.length == 0 ? "" : ("&" + params.join("&"));
 }
 
 function saveQueryInWindow() {
-    window.name = CueCard.jsonize(
-        {
-            t: c.queryEditor.content(),
-            e: c.controlPane.getQueryEnvelope({}, true)
-        },
-        {
-            breakLines: false
-        }
-    );
+  window.name = CueCard.jsonize(
+    {
+      t: c.queryEditor.content(),
+      e: c.controlPane.getQueryEnvelope({}, true)
+    },
+    {
+      breakLines: false 
+    }
+  );
 }
 
 function computeTinyCompactLink() {
--- trunk/www/queryeditor/queryeditor.mf.js
+++ trunk/www/queryeditor/queryeditor.mf.js
@@ -2,5 +2,6 @@
   "lib/jquerytools/tabs.js",
   "lib/jqueryui/jquery.ui.core.mf.js",
   "lib/jqueryui/jquery.ui.dialog.js",
-  "index.js",
+  "lib/template/jquery.collapse_module.js",
+  "index.js"
 ]

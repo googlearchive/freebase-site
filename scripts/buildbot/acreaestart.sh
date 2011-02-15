d8e26b177355aa19c2e53c0bc7af9a3ff2bcde5b
bneutra@google.com <bneutra@google.com@ff54e9e8-a3ca-d32d-4494-0c755e69c161>
2011-02-15 21:00:21 +0000

    find the appengine process

--- scripts/buildbot/acreaestart.sh
+++ scripts/buildbot/acreaestart.sh
@@ -6,7 +6,7 @@ mkdir -p _logs
 mkdir -p _data/acre
 nohup sh -c "exec ${RUN_CMD} >> _logs/acre.console.log 2>&1" >/dev/null &
 sleep 10
-A_PID=`ps ax | grep 'acreae/build/_build/war' | grep -v grep | awk '{print $1}'`
+A_PID=`ps ax | grep 'acreae/build/webapp' | grep -v grep | awk '{print $1}'`
 ACRE_PID="_data/acre/acre.pid"
 echo ${A_PID} > ${ACRE_PID}
 #./acre -c acreaebot appengine-prepare-tests

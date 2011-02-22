94cb368c19bbdeb6ab7cd7312a895ba48d9d009b
bneutra@google.com <bneutra@google.com@ff54e9e8-a3ca-d32d-4494-0c755e69c161>
2011-02-22 19:23:50 +0000

    add logoutput

--- trunk/scripts/buildbot/acreaebotsetup.sh
+++ trunk/scripts/buildbot/acreaebotsetup.sh
@@ -2,7 +2,8 @@
 cd `dirname $0`
 SRC=`pwd`
 BASE=~/acreaebot/acreae/build
-#${BASE}/acre get-mysql-driver
+# update freebase-site to latest known passing version
+svn up -r `cat ~/buildbot/freebase-site.latest` ~/freebase-site/
 ${SRC}/sitesetup.sh ~/acreaebot/acreae/build ~/freebase-site
 cp ${SRC}/project.acreaebot.conf ${BASE}/config
 ${BASE}/acre distclean

b190d28f8c546d897c50db660463ef01b16c6259
daepark@google.com <daepark@google.com@ff54e9e8-a3ca-d32d-4494-0c755e69c161>
2011-02-23 23:11:08 +0000

    checkpoint for prop/value context menus

--- www/topic/topic.js
+++ www/topic/topic.js
@@ -30,51 +30,107 @@
  */
 ;(function($, fb) {
 
-  $(function() {
-
-    // Init keyboard shortcuts @see kbs.js
-    var kbs = fb.kbs.init("#topic-data");
-    kbs.next_domain(true);
-    $("#topic-data .kbs").live("click", function() {
-      // set current on click
-      var current = kbs.get_current();
-      kbs.set_next(current, $(this), true);
-    });
-
-    // Initialize filter menu collapse/expand
-    $(".column.nav > .module").collapse_module(".section");
-
-    // Initialize prop counts filter suggest input
-    fb.filters.init_domain_type_property_filter(".column.nav");
-
-    // Initialize the property limit slider
-    fb.filters.init_limit_slider_filter("#limit-slider", 10, 1, 100, 1);
-
-    $(".toolbar-trigger").click(function(){
-      var $add_type_pane = $(".add-type").first();
-      var $toolbar = $(this).closest(".toolbar");
-      var $trigger = $(this);
-
-      if($add_type_pane.is(":visible")) {
-        $toolbar.removeClass("active");
-        $trigger.removeClass("active");
-        $add_type_pane.slideUp();
-      }
-      else {
-        $trigger.addClass("active");
-        $toolbar.addClass("active");
-        $add_type_pane.slideDown();
-      }
-    });
-  });
-
-  $(".sub-foobar-label").tooltip({
-    relative: true,
-    position: 'bottom center',
-    events: {
-      def: 'click, mouseout'
+  var topic = fb.topic = {
+    init_row_menu: function(context) {
+      var row_menu = $("#row-menu");
+      $(".menu-trigger", context).each(function() {
+        $(this).tooltip({
+          events: {def: "click,mouseout"},
+          position: "bottom right",
+          offset: [-10, -10],
+          effect: "fade",
+          delay: 300,
+          tip: "#row-menu",
+          onBeforeShow: function() {
+            var trigger = this.getTrigger();
+            var md = trigger.parents(".kbs:first").metadata();
+            row_menu.empty();
+            $.each(md.m, function(i, menu) {
+              var li = $('<li class="row-menu-item">');
+              var a = $('<a>').text(menu.text);
+              $.each(menu.attrs, function(k,v) {
+                a.attr(k, v);
+              });
+              li.append(a);
+              row_menu.append(li);
+            });
+            row_menu.data("trigger", trigger);
+          }
+        });
+      });
     },
-    delay: 500
-  });
 
+    init: function() {
+      // Init keyboard shortcuts @see kbs.js
+      var kbs = fb.kbs.init("#topic-data");
+      kbs.next_domain(true);
+      $("#topic-data .kbs")
+        .live("click", function() {
+           // set current on click
+           var current = kbs.get_current();
+           kbs.set_next(current, $(this), true);
+        });
+
+      // init row context menu
+      topic.init_row_menu("#topic-data");
+
+      // Initialize filter menu collapse/expand
+      $(".column.nav > .module").collapse_module(".section");
+
+      // Initialize prop counts filter suggest input
+      fb.filters.init_domain_type_property_filter(".column.nav");
+
+      // Initialize the property limit slider
+      fb.filters.init_limit_slider_filter("#limit-slider", 10, 1, 100, 1);
+
+      $(".toolbar-trigger").click(function(){
+        var $add_type_pane = $(".add-type").first();
+        var $toolbar = $(this).closest(".toolbar");
+        var $trigger = $(this);
+
+        if($add_type_pane.is(":visible")) {
+          $toolbar.removeClass("active");
+          $trigger.removeClass("active");
+          $add_type_pane.slideUp();
+        }
+        else {
+          $trigger.addClass("active");
+          $toolbar.addClass("active");
+          $add_type_pane.slideDown();
+        }
+      });
+    },
+
+    prop_edit: function(e) {
+      var trigger = $("#row-menu").data("trigger");
+      topic.prop_edit_trigger(trigger);
+      return false;
+    },
+
+    prop_add: function(e) {
+      var trigger = $("#row-menu").data("trigger");
+      topic.prop_add_trigger(trigger);
+      return false;
+    },
+
+    value_edit: function(e) {
+      var trigger =  $("#row-menu").data("trigger");
+      topic.value_edit_trigger(trigger);
+    },
+
+    prop_edit_trigger: function(trigger) {
+      console.log("prop_edit_trigger", trigger.parents(".kbs:first").metadata());
+    },
+
+    prop_add_trigger: function(trigger) {
+      console.log("prop_add_trigger", trigger.parents(".kbs:first").metadata());
+    },
+
+    value_edit_trigger: function(trigger) {
+      console.log("value_edit_trigger", trigger.parents(".kbs:first").metadata());
+    }
+  };
+
+
+  $(topic.init);
 })(jQuery, window.freebase);

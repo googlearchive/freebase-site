d5ff774194371db97d8269ceceb77790a2cc9af6
bneutra@google.com <bneutra@google.com@ff54e9e8-a3ca-d32d-4494-0c755e69c161>
2011-03-01 19:54:41 +0000

    log at FINE level

--- trunk/scripts/buildbot/acreaestart.sh
+++ trunk/scripts/buildbot/acreaestart.sh
@@ -3,7 +3,7 @@ BASE=~/acreaebot/acreae/build
 cd $BASE
 # die!
 ps ax | grep 'acreae/build/webapp' | grep -v grep | awk '{print $1}' | xargs kill
-RUN_CMD="./acre -c acreaebot appengine-run"
+RUN_CMD="./acre -v $1 -c acreaebot appengine-run"
 mkdir -p _logs
 mkdir -p _data/acre
 nohup sh -c "exec ${RUN_CMD} >> _logs/acre.console.log 2>&1" >/dev/null &

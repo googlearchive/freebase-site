6ff8557d3559abcdbc084923a4c17ed97a37543c
masouras@google.com <masouras@google.com@ff54e9e8-a3ca-d32d-4494-0c755e69c161>
2011-03-23 19:33:11 +0000

    [sitedeploy] Creating tag 3a

---
+++ tags/www/topic/3a/topic.js
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2010, Google Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following disclaimer
+ * in the documentation and/or other materials provided with the
+ * distribution.
+ *     * Neither the name of Google Inc. nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+;(function($, fb, propbox) {
+
+  var topic = fb.topic = {
+
+    init: function() {
+
+      propbox.init("#topic-data", {
+        id: fb.c.id,
+        base_url: fb.h.ajax_url("lib/propbox"),
+        lang: fb.lang || "/lang/en",
+        suggest: {
+          service_url: fb.h.legacy_fb_url(),
+          service_path: "/private/suggest",
+          flyout_service_url: fb.h.legacy_fb_url(),
+          flyout_service_path: "/private/flyout",
+          mqlread_url: fb.h.fb_api_url("/api/service/mqlread"),
+          category: "object",
+          type: "/common/topic",
+          status: ["", "Searching...", "Select an item from the list:"]
+        }
+      });
+
+      // Initialize filter menu collapse/expand
+      $(".column.nav > .module").collapse_module(".section");
+
+      // Initialize prop counts filter suggest input
+      fb.filters.init_domain_type_property_filter(".column.nav");
+
+      // Initialize the property limit slider
+      fb.filters.init_limit_slider_filter("#limit-slider", 10, 1, 100, 1);
+
+      $(".toolbar-trigger").click(function(){
+        var $add_type_pane = $(".add-type").first();
+        var $toolbar = $(this).closest(".toolbar");
+        var $trigger = $(this);
+
+        if($add_type_pane.is(":visible")) {
+          $toolbar.removeClass("active");
+          $trigger.removeClass("active");
+          $add_type_pane.slideUp();
+        }
+        else {
+          $trigger.addClass("active");
+          $toolbar.addClass("active");
+          $add_type_pane.slideDown();
+        }
+      });
+    }
+  };
+
+
+  $(topic.init);
+})(jQuery, window.freebase, window.propbox);

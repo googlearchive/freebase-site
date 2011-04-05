5f6910f49cf64880ef38a7f425693ef1761edbb9
bneutra@google.com <bneutra@google.com@ff54e9e8-a3ca-d32d-4494-0c755e69c161>
2011-04-05 19:37:07 +0000

    fix test result parsing for nose tests. implement locking so only one build runs at a time

--- scripts/buildbot/acreaestart.sh
+++ scripts/buildbot/acreaestart.sh
@@ -6,7 +6,7 @@ ps ax | grep 'acreae/build/webapp' | grep -v grep | awk '{print $1}' | xargs kil
 RUN_CMD="./acre -v $1 -c acreaebot appengine-run"
 mkdir -p _logs
 mkdir -p _data/acre
-nohup sh -c "exec ${RUN_CMD} >> _logs/acre.console.log 2>&1" >/dev/null &
+nohup sh -c "exec ${RUN_CMD} >> acre.console.log 2>&1" >/dev/null &
 sleep 10
 A_PID=`ps ax | grep 'acreae/build/webapp' | grep -v grep | awk '{print $1}'`
 ACRE_PID="_data/acre/acre.pid"

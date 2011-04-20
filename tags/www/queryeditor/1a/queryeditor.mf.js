6ea587c35586be2347257f87432267ccb48e1e52
masouras@google.com <masouras@google.com@ff54e9e8-a3ca-d32d-4494-0c755e69c161>
2011-04-20 22:49:33 +0000

    [sitedeploy] Creating tag 1a

---
+++ tags/www/queryeditor/1a/queryeditor.mf.js
@@ -0,0 +1,8 @@
+[
+  "lib/jquerytools/tabs.js",
+  "lib/jqueryui/jquery.ui.core.mf.js",
+  "lib/jqueryui/jquery.ui.dialog.js",
+  "lib/template/jquery.nicemenu.js",
+  "lib/template/jquery.collapse_module.js",
+  "queryeditor.js"
+]

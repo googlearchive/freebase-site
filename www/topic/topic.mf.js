07b5f841fb887e4b262786009eaba5c7078384c4
jasondouglas@google.com <jasondouglas@google.com@ff54e9e8-a3ca-d32d-4494-0c755e69c161>
2011-04-24 19:07:42 +0000

    Don't load collapse_modules twice

--- www/topic/topic.mf.js
+++ www/topic/topic.mf.js
@@ -1,7 +1,6 @@
 [
   "lib/jquerytools/toolbox.expose.js",
   "lib/jquerytools/overlay.js",
-  "lib/template/jquery.collapse_module.js",
   "lib/filter/filters.mf.js",
   "lib/propbox/propbox.mf.js",
   "topic.js"

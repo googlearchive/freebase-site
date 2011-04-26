02eae7d8cebcbc19fcc3a0530087921504203f3a
daepark@google.com <daepark@google.com@ff54e9e8-a3ca-d32d-4494-0c755e69c161>
2011-04-26 02:52:00 +0000

    edit localed name rountrip with mqlwrite

--- www/lib/i18n/i18n-edit.js
+++ www/lib/i18n/i18n-edit.js
@@ -49,12 +49,15 @@
             form: html,
             ajax: {
               base_url: base_url,
-              data: submit_data,
+              data: $.extend({}, submit_data, {"insert": [], "delete": []}),
               url: base_url + "/text_edit_submit.ajax"
             },
-            input: $(".data-input:first", html).data_input(),
-            lang_select: $(".lang-select", html)
+            // add new input elements
+            add_input: $(".data-input:first", html).data_input(),
+            add_lang: $(".lang-select", html)
           };
+          // unique property?
+          form.unique = form.add_lang.is(".unique");
           i18n.edit.text_edit_init(form);
         },
         error: function(xhr) {
@@ -76,44 +79,68 @@
           color: '#000',
           loadSpeed: 200,
           opacity: 0.5
+        },
+        onLoad: function() {
+          $(":text:first", form.form).focus();
         }
       });
       var event_prefix = "i18n.edit.text_edit.";
       $(".button-submit", form.form).click(function() {
         form.form.trigger(event_prefix + "submit");
       });
-      $(".icon-link.delete", form.form).click(function(e) {
-        var row = $(this).parents("tr:first");
-        form.form.trigger(event_prefix + "delete", row);
+
+      function value_row_init(row) {
+        $(".icon-link.delete", row).click(function(e) {
+          form.form.trigger(event_prefix + "delete", row);
+        });
+        $(".data-input", row)
+          .data_input()
+          .bind("submit", function() {
+            form.form.trigger(event_prefix + "submit");
+          });
+      };
+
+      $(".values > tr", form.form).each(function() {
+        value_row_init($(this));
       });
+
       $(".icon-link.add", form.form).click(function(e) {
         form.form.trigger(event_prefix + "add");
       });
-      form.lang_select
-        .keypress(function(e) {
-          if (e.keyCode === 13) {
-            form.form.trigger(event_prefix + "add");
-          }
-        });
-      form.input.bind("submit", function() {
+      form.add_lang.keypress(function(e) {
+        if (e.keyCode === 13) {
+          form.form.trigger(event_prefix + "add");
+        }
+      });
+      form.add_input.bind("submit", function() {
         form.form.trigger(event_prefix + "add");
       });
 
       form.form
         .bind(event_prefix + "add", function() {
-          var name_value = form.input.data("name_value");
-          var lang_id = form.lang_select.val();
-          if (name_value && lang_id && name_value[1] != "") {
-            var option = $("option[value=" + lang_id.replace(/\//g, "\\/") + "]", form.lang_select);
+          var name_value = form.add_input.data("name_value");
+          var lang = form.add_lang.val();
+          if (name_value && lang) {
+            var value = $.trim(name_value[1]);
+            if (value === "") {
+              return;
+            }
+
+            i18n.edit.text_edit_insert(form, value, lang);
+
+            var option = $("option[value=" + lang.replace(/\//g, "\\/") + "]", form.add_lang);
             var lang_name = option.text();
-            var new_row = i18n.edit.new_text_edit_row(name_value[1], lang_id, lang_name).hide();
+            var new_row = i18n.edit.new_text_edit_row(value, lang, lang_name).hide();
             $("tbody.values").prepend(new_row);
+            value_row_init(new_row);
             new_row.fadeIn(function() {
-              form.input.data("$.data_input").reset();
-              form.lang_select[0].selectedIndex = 0;
-              // disable only if unique property
-              option.attr("disabled", "disabled");
-              $(":text", form.input).focus();
+              form.add_input.data("$.data_input").reset();
+              form.add_lang[0].selectedIndex = 0;
+              // disable lang option if unique property
+              if (form.unique) {
+                option.attr("disabled", "disabled");
+              }
+              $(":text", form.add_input).focus();
             });
           }
           else {
@@ -122,21 +149,101 @@
           }
         })
         .bind(event_prefix + "submit", function() {
-
+          i18n.edit.text_edit_submit(form);
         })
         .bind(event_prefix + "delete", function(e, row) {
           row = $(row);
           var lang = $(".lang", row).attr("data-value");
+          var value = $.trim($(".fb-input", row).data("$.validate_input").original_value);
+          if (value !== "") {
+            i18n.edit.text_edit_delete(form, value, lang);
+          }
           row.fadeOut(function() {
-            // re-enable option[value=lang]
-            $("option[value=" + lang.replace(/\//g, "\\/") + "]", form.lang_select).removeAttr("disabled");
+            // re-enable option[value=lang] if unique property
+            if (form.unique) {
+              $("option[value=" + lang.replace(/\//g, "\\/") + "]", form.add_lang).removeAttr("disabled");
+            }
             $(this).remove();
           });
         });
     },
 
+    text_edit_insert: function(form, value, lang) {
+      form.ajax.data["insert"].push({value:value, lang:lang});
+      form.ajax.data["delete"] = $.grep(form.ajax.data["delete"], function(v) {
+        return v.value === value && v.lang === lang;
+      }, true);
+    },
 
-    new_text_edit_row: function(value, lang_id, lang_name) {
+    text_edit_delete: function(form, value, lang) {
+      form.ajax.data["delete"].push({value:value, lang:lang});
+      form.ajax.data["insert"] = $.grep(form.ajax.data["insert"], function(v) {
+        return v.value === value && v.lang === lang;
+      }, true);
+    },
+
+    text_edit_submit: function(form) {
+      // are we already submitting?
+      if (form.form.is(".loading")) {
+        return;
+      }
+      // submit button enabled?
+      var button_submit = $(".button-submit", form.form);
+      if (button_submit.is(":disabled")) {
+        return;
+      }
+      // remove focus from activeElement
+      if (document.activeElement) {
+        $(document.activeElement).blur();
+      }
+      $(".values .data-input", form.form).each(function() {
+        var name_value = $(this).data("name_value");
+        if (name_value) {
+          var value = name_value[1];
+          if (value === "") {
+            return;
+          }
+          var original_value = $.trim($(".fb-input", this).data("$.validate_input").original_value);
+          if (value !== original_value) {
+            var lang = $(this).parents("tr:first").find(".lang").attr("data-value");
+            i18n.edit.text_edit_delete(form, original_value, lang);
+            i18n.edit.text_edit_insert(form, value, lang);
+          }
+        }
+      });
+
+      //console.log("text_edit_submit DELETE", JSON.stringify(form.ajax.data["delete"], null, 2));
+      //console.log("text_edit_submit INSERT", JSON.stringify(form.ajax.data["insert"], null, 2));
+
+      if (form.ajax.data["delete"].length ||
+          form.ajax.data["insert"].length) {
+        form.form.addClass("loading");
+        button_submit.addClass("disabled").attr("disabled", "disabled");
+        var submit_data = $.extend({}, form.ajax.data, {
+          "delete": JSON.stringify(form.ajax.data["delete"]),
+          "insert": JSON.stringify(form.ajax.data["insert"])
+        });
+        $.ajax({
+          url: form.ajax.url,
+          type: "POST",
+          dataType: "json",
+          data: submit_data,
+          success: function(data, status, xhr) {
+            if (data.code !== "/api/status/ok") {
+              // TODO: handle error
+              return console.error("text_edit_submt", xhr);
+            }
+            window.location.reload(true);
+          },
+          error: function(xhr) {
+            // TODO: handle error
+            console.error("text_edit_submt", xhr);
+          }
+        });
+      }
+    },
+
+    new_text_edit_row: function(value, lang, lang_name) {
       var row =
         $('<tr>' +
           '  <th class="row-header" scope="row">' +
@@ -152,10 +259,11 @@
           '  </td>' +
           '</tr>');
       $(":text", row).val(value);
-      $(".lang", row).attr("data-value", lang_id).text(lang_name);
+      $(".lang", row).attr("data-value", lang).text(lang_name);
       return row;
     }
 
   };
 
 })(jQuery, window.i18n);
+

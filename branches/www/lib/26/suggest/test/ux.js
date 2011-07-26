1611649265f43185b1b3e4512b20c61936155723
masouras@google.com <masouras@google.com@ff54e9e8-a3ca-d32d-4494-0c755e69c161>
2011-07-26 19:48:52 +0000

    [sitedeploy] Creating branch lib:26

---
+++ branches/www/lib/26/suggest/test/ux.js
@@ -0,0 +1,208 @@
+$(function() {
+    module("ux", module_options);
+
+    function test_select(name, onshow) {
+      test("select: " + name, 3, function() {
+             var o = $.extend({}, default_options, {filter:"(any type:/music/artist)"});
+             test_input1.suggest(o);
+             var inst = get_instance();
+
+             stop();
+             var t = test_timeout();
+             test_input1
+               .bind("fb-select", function(e, data) {
+                       clearTimeout(t);
+                       ok(data);
+                       equals(data.name, "Bob Dylan");
+                       equals(data.id, bob_dylan_id);
+                       start();
+                     })
+               .bind("fb-pane-show", function() {
+                       if ($(">li", inst.list).length) {
+                         onshow();
+                       }
+                     })
+               .focus()
+               .val("bob dylan")
+               .trigger("textchange");
+           });
+    };
+
+    test_select("MOUSEOVER MOUSECLICK", function() {
+                  var inst = get_instance();
+                  var first = $("li:first", inst.list).simulate("mouseover");
+                  simulate_mouseclick(first);
+                });
+
+    test_select("MOUSEOVER ENTER", function() {
+                  var inst = get_instance();
+                  $("li:first", inst.list).simulate("mouseover");
+                  simulate_keypress(test_input1, $.simulate.VK_ENTER);
+                });
+
+    test_select("ENTER ENTER", function() {
+                  simulate_keypress(test_input1, $.simulate.VK_ENTER);
+                  simulate_keypress(test_input1, $.simulate.VK_ENTER);
+                });
+
+    test_select("DOWN ENTER", function() {
+                  simulate_keypress(test_input1, $.simulate.VK_DOWN);
+                  simulate_keypress(test_input1, $.simulate.VK_ENTER);
+                });
+
+    test_select("UP DOWN DOWN ENTER", function() {
+                  simulate_keypress(test_input1, $.simulate.VK_UP);
+                  simulate_keypress(test_input1, $.simulate.VK_DOWN);
+                  simulate_keypress(test_input1, $.simulate.VK_DOWN);
+                  simulate_keypress(test_input1, $.simulate.VK_ENTER);
+                });
+
+    test_select("CTRL+UP ENTER", function() {
+                  simulate_keypress(test_input1, {keyCode:$.simulate.VK_UP, ctrlKey: true});
+                  simulate_keypress(test_input1, $.simulate.VK_ENTER);
+                });
+
+    test_select("CTRL+DOWN DOWN DOWN ENTER", function() {
+                  simulate_keypress(test_input1, {keyCode:$.simulate.VK_DOWN, ctrlKey: true});
+                  simulate_keypress(test_input1, $.simulate.VK_DOWN);
+                  simulate_keypress(test_input1, $.simulate.VK_DOWN);
+                  simulate_keypress(test_input1, $.simulate.VK_ENTER);
+                });
+
+    test("select: flyout", 3, function() {
+           var o = $.extend({}, default_options, {filter:"(any type:/music/artist)"});
+           test_input1.suggest(o);
+           var inst = get_instance();
+
+           stop();
+           var t = test_timeout();
+           test_input1
+             .bind("fb-select", function(e, data) {
+                     clearTimeout(t);
+                     ok(data);
+                     equals(data.name, "Bob Dylan");
+                     equals(data.id, bob_dylan_id);
+                     start();
+                   })
+             .bind("fb-flyoutpane-show", function() {
+                     simulate_mouseclick(inst.flyoutpane);
+                   })
+             .bind("fb-pane-show", function() {
+                     if ($(">li", inst.list).length) {
+                       simulate_keypress(test_input1, $.simulate.VK_DOWN);
+                     }
+                   })
+             .focus()
+             .val("bob dylan")
+             .trigger("textchange");
+         });
+
+
+    test("suggest_new: SHIFT+ENTER", 1, function() {
+           var o = $.extend({}, default_options, {suggest_new:"new"});
+           test_input1.suggest(o);
+           var inst = get_instance();
+
+           stop();
+           var t = test_timeout();
+           test_input1
+             .bind("fb-select-new", function(e, val) {
+                     clearTimeout(t);
+                     equals(val, "bob dylan");
+                     start();
+                   })
+             .bind("fb-pane-show", function() {
+                     if ($(">li", inst.list).length) {
+                       simulate_keypress(test_input1, {keyCode: $.simulate.VK_ENTER, shiftKey: true});
+                     }
+                   })
+             .focus()
+             .val("bob dylan")
+             .trigger("textchange");
+         });
+
+
+    test("more: CLICK", 1, function() {
+           var o = $.extend({}, default_options, {filter:"(any type:/music/artist)"});
+           test_input1.suggest(o);
+           var inst = get_instance();
+
+           var more = false,
+           count = 0;
+
+           stop();
+           var t = test_timeout();
+           test_input1
+             .bind("fb-pane-show", function() {
+                     if (more) {
+                       clearTimeout(t);
+                       ok($(">li", inst.list).length > count, "got more");
+                       start();
+                     }
+                     else {
+                       count = $(">li", inst.list).length;
+                       if (count) {
+                         $(".fbs-more-link", inst.pane).click();
+                         more = true;
+                       }
+                     }
+                   })
+             .focus()
+             .val("bob dylan")
+             .trigger("textchange");
+         });
+
+    test("more: CTRL+M", 1, function() {
+           var o = $.extend({}, default_options, {filter:"(any type:/music/artist)"});
+           test_input1.suggest(o);
+           var inst = get_instance();
+
+           var more = false,
+           count = 0;
+
+           stop();
+           var t = test_timeout();
+           test_input1
+             .bind("fb-pane-show", function() {
+                     if (more) {
+                       clearTimeout(t);
+                       ok($(">li", inst.list).length > count, "got more");
+                       start();
+                     }
+                     else {
+                       count = $(">li", inst.list).length;
+                       if (count) {
+                         simulate_keypress(test_input1, {keyCode: 77, ctrlKey: true});
+                         more = true;
+                       }
+                     }
+                   })
+             .focus()
+             .val("bob dylan")
+             .trigger("textchange");
+         });
+
+
+    test("cancel: ESC", 2, function() {
+           var o = $.extend({}, default_options, {filter:"(any type:/music/artist)"});
+           test_input1.suggest(o);
+           var inst = get_instance();
+
+           stop();
+           var t = test_timeout();
+           test_input1
+             .bind("fb-pane-show", function() {
+                     if ($(">li", inst.list).length) {
+                       clearTimeout(t);
+                       ok(inst.pane.is(":visible"), "pane visible");
+                       simulate_keypress(test_input1, $.simulate.VK_ESC);
+                       ok(!inst.pane.is(":visible"), "pane NOT visible");
+                       start();
+                     }
+                   })
+             .focus()
+             .val("bob dylan")
+             .trigger("textchange");
+         });
+
+  });

8b87f00928baaf10204d725a0a8cc1e8be46e1c0
jasondouglas@google.com <jasondouglas@google.com@ff54e9e8-a3ca-d32d-4494-0c755e69c161>
2011-07-26 21:41:50 +0000

    Update to reflect routing changes

--- www/lib/template/helpers.js
+++ www/lib/template/helpers.js
@@ -163,7 +163,6 @@
     reentrant_url: function(prefix, path) {
       path = h.resolve_reentrant_path(path);
       path = path.replace(/^\/\//, prefix + "/");
-      path = path.replace(".svn.freebase-site.googlecode.dev", "");
       var args = Array.prototype.slice.call(arguments, 2);
       args.unshift(path);
       args.unshift(null); // relative url

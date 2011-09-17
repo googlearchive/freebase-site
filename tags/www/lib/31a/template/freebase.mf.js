21a2c7063e9c62f99325074575f07d57bc34b146
masouras@google.com <masouras@google.com@ff54e9e8-a3ca-d32d-4494-0c755e69c161>
2011-09-17 01:10:25 +0000

    [sitedeploy] Creating tag 31a

---
+++ tags/www/lib/31a/template/freebase.mf.js
@@ -0,0 +1,16 @@
+[
+  "template/json2.js",
+  "template/jquery.factory.js",
+  "template/jquery.cookie.js",
+  "template/jquery.localstore.js",
+  "template/jquery.placeholder.js",
+  "template/jquery.metadata.js",
+  "template/tablesorter.js",
+  "jqueryui/jquery.ui.core.mf.js",
+  "template/freebase.js",
+  "template/helpers.js",
+  "permission/permission.js",
+  "incompatible_types/incompatible-types.js",
+  "i18n/i18n.js",
+  "flag/flag.js"
+]

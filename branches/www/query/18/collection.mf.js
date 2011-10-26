63747ce3ac68b95dcd78c66f3764ab43e124ec10
masouras@google.com <masouras@google.com@ff54e9e8-a3ca-d32d-4494-0c755e69c161>
2011-10-26 22:11:38 +0000

    [sitedeploy] Creating branch query:18

---
+++ branches/www/query/18/collection.mf.js
@@ -0,0 +1,9 @@
+[
+  "lib/jquerytools/toolbox.expose.js",
+  "lib/jquerytools/overlay.js",
+  "lib/filter/filters.mf.js",
+  "lib/propbox/propbox.mf.js",
+  "lib/template/jquery.infinitescroll.js",
+  "lib/collection/collection.js",
+  "collection.js"
+]

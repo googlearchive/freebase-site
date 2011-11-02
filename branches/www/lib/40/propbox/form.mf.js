8e42a8258e37250c23332edc6850deee00c62a82
masouras@google.com <masouras@google.com@ff54e9e8-a3ca-d32d-4494-0c755e69c161>
2011-11-02 01:01:27 +0000

    [sitedeploy] Creating branch lib:40

---
+++ branches/www/lib/40/propbox/form.mf.js
@@ -0,0 +1,8 @@
+[
+  "jquerytools/toolbox.expose.js",
+  "jquerytools/overlay.js",
+  "propbox/jquery.mqlkey.js",
+  "propbox/jquery.validate_input.js",
+  "propbox/jquery.data_input.js",
+  "propbox/form.js"
+]

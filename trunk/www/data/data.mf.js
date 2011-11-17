b4b90f3196f1053176ccac4e48c88dd8ae6dbab5
daepark@google.com <daepark@google.com@ff54e9e8-a3ca-d32d-4494-0c755e69c161>
2011-11-17 00:03:56 +0000

    add propbox.mf.js to data.mf.js. needed for collection editing

--- trunk/www/data/data.mf.js
+++ trunk/www/data/data.mf.js
@@ -2,6 +2,7 @@
   "lib/flot/flot.core.mf.js",
   "lib/jquerytools/tabs.js",
   "lib/template/tablesorter.js",
+  "lib/propbox/propbox.mf.js",
   "lib/template/jquery.infinitescroll.js",
   "lib/collection/collection.js",
   "data.js"

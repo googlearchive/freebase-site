b5c81b13cf11f09647216eb70093f1cff9275ef6
daepark@google.com <daepark@google.com@ff54e9e8-a3ca-d32d-4494-0c755e69c161>
2012-02-03 21:30:45 +0000

    Issue 5604052: Add/edit article propbox support

--- trunk/www/lib/propbox/propbox-edit.js
+++ trunk/www/lib/propbox/propbox-edit.js
@@ -49,59 +49,73 @@
      *
      * Add a new value to a property (topic, literal, cvt)
      */
-    prop_add_begin: function(prop_section, unique) {
-      var submit_data = {
-        s: propbox.options.id,
-        p: prop_section.attr("data-id"),
-        lang: propbox.options.lang
-      };
-      var ls = $(">.data-section", prop_section);
-      //$("> .data-table > tbody > .empty-row, > .data-list > .empty-row", ls).hide();
-      var body = $("> .data-table > tbody, > .data-list", ls);
-
+    prop_add_begin: function(prop_section) {
       $.ajax($.extend(formlib.default_begin_ajax_options(), {
         url: propbox.options.base_ajax_url + "/prop_add_begin.ajax",
-        data: submit_data,
+        data: {
+          s: propbox.options.id,
+          p: prop_section.attr("data-id"),
+          lang: propbox.options.lang
+        },
         onsuccess: function(data) {
           var html = $(data.result.html);
-          var head_row = $(".edit-row-head", html);
-          var edit_row = $(".edit-row", html);
-          var submit_row = $(".edit-row-submit", html);
-          var event_prefix = "propbox.edit.prop_add.";
-          var options = {
-            event_prefix: event_prefix,
-            // callbacks
-            init: edit.init_prop_add_form,
-            validate: edit.validate_prop_add_form,
-            submit:  edit.submit_prop_add_form,
-            reset: edit.reset_prop_add_form,
-            // submit ajax options
-            ajax: {
-              url: propbox.options.base_ajax_url + "/prop_edit_submit.ajax",
-              data: submit_data
-            },
-            // jQuery objects
-            body: body,
-            head_row: head_row,
-            edit_row: edit_row,
-            submit_row: submit_row,
-
-            structure: html.metadata()
-          };
-          edit_row
-            .bind(event_prefix + "cancel", function(e) {
-              prop_section.removeClass("editing");
-            })
-            .bind(event_prefix + "success", function() {
-              if (unique) {
-                edit_row.trigger(form.event_prefix + "cancel");
-              }
-            });
-          formlib.init_inline_add_form(options);
+          var structure = html.metadata();
+          var ect = structure.expected_type.id;
+          if (ect === "/common/document") {
+              // modal document editing
+              return edit.prop_add_document(prop_section, html, structure);
+          }
+          else if (ect === "/common/image") {
+              // modal image editing
+              return edit.prop_add_image(prop_section, html, structure);
+          }
+          else {
+              // default inline editing
+              return edit.prop_add(prop_section, html, structure);
+          }
         }
       }));
     },
 
+    prop_add: function(prop_section, html, structure) {
+      var ls = $(">.data-section", prop_section);
+      //$("> .data-table > tbody > .empty-row, > .data-list > .empty-row", ls).hide();
+      var body = $("> .data-table > tbody, > .data-list", ls);
+
+      var head_row = $(".edit-row-head", html);
+      var edit_row = $(".edit-row", html);
+      var submit_row = $(".edit-row-submit", html);
+      var event_prefix = "propbox.edit.prop_add.";
+      var options = {
+        event_prefix: event_prefix,
+        // callbacks
+        init: edit.init_prop_add_form,
+        validate: edit.validate_prop_add_form,
+        submit:  edit.submit_prop_add_form,
+        reset: edit.reset_prop_add_form,
+        // submit ajax options
+        ajax: {
+          url: propbox.options.base_ajax_url + "/prop_edit_submit.ajax"
+        },
+        // jQuery objects
+        body: body,
+        head_row: head_row,
+        edit_row: edit_row,
+        submit_row: submit_row,
+
+        structure: structure
+      };
+      edit_row
+        .bind(event_prefix + "cancel", function(e) {
+          prop_section.removeClass("editing");
+        })
+        .bind(event_prefix + "success", function() {
+          if (structure.unique) {
+            edit_row.trigger(form.event_prefix + "cancel");
+          }
+        });
+      formlib.init_inline_add_form(options);
+    },
 
     init_prop_add_form: function(options) {
       edit.init_data_inputs(options);
@@ -171,55 +185,69 @@
         var prop_value = $(".property-value:first", prop_row);
         value = prop_value.attr("data-id") || prop_value.attr("data-value") || prop_value.attr("datetime");
       }
-      var submit_data = {
-        s: propbox.options.id,
-        p: prop_section.attr("data-id"),
-        replace: value,
-        lang: propbox.options.lang
-      };
-
       $.ajax($.extend(formlib.default_begin_ajax_options(), {
         url: propbox.options.base_ajax_url +  "/value_edit_begin.ajax",
-        data: submit_data,
+        data: {
+          s: propbox.options.id,
+          p: prop_section.attr("data-id"),
+          replace: value,
+          lang: propbox.options.lang
+        },
         onsuccess: function(data) {
           var html = $(data.result.html);
-          var head_row = $(".edit-row-head", html);
-          var edit_row = $(".edit-row", html);
-          var submit_row = $(".edit-row-submit", html);
-          var event_prefix = "propbox.edit.value_edit.";
-          var options = {
-            event_prefix: event_prefix,
-            // callbacks
-            init: edit.init_value_edit_form,
-            validate: edit.validate_value_edit_form,
-            submit: edit.submit_value_edit_form,
-            // submit ajax_options
-            ajax: {
-              url:  propbox.options.base_ajax_url + "/prop_edit_submit.ajax",
-              data: submit_data
-            },
-            // jQuery objects
-            row: prop_row,
-            head_row: head_row,
-            edit_row: edit_row,
-            submit_row: submit_row,
-
-            structure: html.metadata()
-          };
-          edit_row
-            .bind(event_prefix + "success", function(e) {
-              prop_section.removeClass("editing");
-            })
-            .bind(event_prefix + "cancel", function(e) {
-              prop_section.removeClass("editing");
-            })
-            .bind(event_prefix + "delete", function() {
-              edit_row.trigger(event_prefix + "cancel");
-              propbox.edit.value_delete_begin(prop_section, prop_row);
-            });
-          formlib.init_inline_edit_form(options);
+          var structure = html.metadata();
+          var ect = structure.expected_type.id;
+          if (ect === "/common/document") {
+             // modal document editing
+              return edit.value_edit_document(prop_section, prop_row, html, structure);
+          }
+          else if (ect === "/common/image") {
+              // modal image editing
+              return edit.value_edit_image(prop_section, prop_row, html, structure);
+          }
+          else {
+              // default inline editing
+              return edit.value_edit(prop_section, prop_row, html, structure);
+          }
         }
       }));
+    },
+      
+    value_edit: function(prop_section, prop_row, html, structure) {
+      var head_row = $(".edit-row-head", html);
+      var edit_row = $(".edit-row", html);
+      var submit_row = $(".edit-row-submit", html);
+      var event_prefix = "propbox.edit.value_edit.";
+      var options = {
+        event_prefix: event_prefix,
+        // callbacks
+        init: edit.init_value_edit_form,
+        validate: edit.validate_value_edit_form,
+        submit: edit.submit_value_edit_form,
+        // submit ajax_options
+        ajax: {
+          url:  propbox.options.base_ajax_url + "/prop_edit_submit.ajax"
+        },
+        // jQuery objects
+        row: prop_row,
+        head_row: head_row,
+        edit_row: edit_row,
+        submit_row: submit_row,
+
+        structure: structure
+      };
+      edit_row
+        .bind(event_prefix + "success", function(e) {
+          prop_section.removeClass("editing");
+        })
+        .bind(event_prefix + "cancel", function(e) {
+          prop_section.removeClass("editing");
+        })
+        .bind(event_prefix + "delete", function() {
+          edit_row.trigger(event_prefix + "cancel");
+          propbox.edit.value_delete_begin(prop_section, prop_row);
+        });
+      formlib.init_inline_edit_form(options);
     },
 
     init_value_edit_form: function(options) {
@@ -306,6 +334,194 @@
       }));
     },
 
+
+    /**
+     * add a /common/document
+     */
+    prop_add_document: function(prop_section, html, structure) {  
+      var event_prefix = "propbox.edit.prop_add_document.";
+      var options = {
+        event_prefix: event_prefix,
+        // callbacks
+        init: edit.init_prop_add_document_form,
+        validate: edit.validate_prop_add_document_form,
+        submit: edit.submit_prop_add_document_form,
+        // submit ajax options
+        ajax: {
+          url: propbox.options.base_ajax_url + 
+            "/prop_add_document_submit.ajax"
+        },
+        // jQuery object
+        prop_section: prop_section,
+        form: html,
+        structure: structure
+      };
+      html
+        .bind(event_prefix + "cancel", function(e) {
+          prop_section.removeClass("editing");
+        });
+      formlib.init_modal_form(options);
+    },
+
+    init_prop_add_document_form: function(options) {
+        var lang_select = $("select[name=lang]", options.form);
+        /**
+         * If selecting a language other than the primary language
+         * (propox.options.lang), display a message that they
+         * may see the article until the page is refreshed with the 
+         * selected language.
+         */
+        lang_select
+            .change(function() {
+                var lang_notice = $(".lang-notice", options.form);
+                if (this.value === propbox.options.lang) {
+                    lang_notice.hide();
+                }
+                else {
+                    $("strong", lang_notice).text($("option:selected", lang_select).text());
+                    lang_notice.show();
+                }
+            });
+        $("textarea", options.form)
+            .bind("keypress", function() {
+                options.form.trigger(options.event_prefix + "valid");
+            })
+            .bind("paste", function() {
+                options.form.trigger(options.event_prefix + "valid");
+            })
+            .focus();
+    },
+
+    validate_prop_add_document_form: function(options) {
+        var textarea = $("textarea", options.form);
+        var val = $.trim(textarea.val());
+        if (val === "") {
+            options.form.trigger(options.event_prefix + "invalid");
+            textarea.focus();
+            return false;
+        }
+        return true;
+    },
+
+    submit_prop_add_document_form: function(options, ajax_options) {
+        var textarea = $("textarea", options.form);
+        ajax_options.data[textarea.attr("name")] = $.trim(textarea.val());
+        ajax_options.data.lang = $("select[name=lang]", options.form).val();
+        $.ajax($.extend(ajax_options, {
+            onsuccess: function(data) {
+                var new_row = $(data.result.html);
+                if (options.prop_row) {
+                    // replace exisiting row
+                    options.prop_row.replaceWith(new_row);                    
+                }
+                else {
+                    // add new row to prop_section
+                    var ls = $(">.data-section", options.prop_section);
+                    var body = $("> .data-table > tbody, > .data-list", ls);
+                    body.append(new_row);                    
+                }
+                i18n.ize(new_row);
+                propbox.init_menus(new_row, true);
+                $(".nicemenu-item.edit").show();
+                propbox.kbs.set_next(null, new_row, true); 
+                options.form.trigger(options.event_prefix + "cancel"); 
+            },
+            onerror: function(errmsg) {
+                options.form.trigger(options.event_prefix + "error", errmsg);
+            }
+        }));
+    },
+
+    /**
+     * edit a /common/document
+     */
+    value_edit_document: function(prop_section, prop_row, html, structure) {
+      var event_prefix = "propbox.edit.value_edit_document.";
+      var options = {
+        event_prefix: event_prefix,
+        // callbacks
+        init: edit.init_value_edit_document_form,
+        validate: edit.validate_value_edit_document_form,
+        submit: edit.submit_value_edit_document_form,
+        // submit ajax options
+        ajax: {
+          url: propbox.options.base_ajax_url + 
+            "/prop_add_document_submit.ajax"
+        },
+        // jQuery object
+        prop_section: prop_section,
+        prop_row: prop_row,
+        form: html,
+        structure: structure,
+
+        // can't edit document modal dialog?
+        cant_edit: html.is(".cant-edit-document-form")
+      };
+      html
+        .bind(event_prefix + "cancel", function(e) {
+          prop_section.removeClass("editing");
+        });
+      if (options.cant_edit) {
+          /**
+           * You can't edit articles with /common/document/source_uri.
+           * If so, the form returned is a modal dialog specifying you
+           * can't edit generated (wikipedia) articles, would you
+           * like to create new?
+           */
+          html.bind(event_prefix + "submit", function(e) {
+              html.trigger(event_prefix + "cancel");
+              edit.prop_add_begin(prop_section);
+          });
+      }
+      formlib.init_modal_form(options);
+    },
+
+    init_value_edit_document_form: function(options) {
+        if (options.cant_edit) {
+            /**
+             * Enable the "Create new" button in the can't edit modal dialog
+             */
+            formlib.enable_submit(options);
+        }
+        else {
+            /**
+             * Otherwise, the same exact form as adding a document
+             */
+            edit.init_prop_add_document_form(options);
+        }
+    },
+
+    validate_value_edit_document_form: function(options) {
+        if (options.cant_edit) {
+            return true;
+        }
+        else {
+            return edit.validate_prop_add_document_form(options);
+        }
+    },
+
+    submit_value_edit_document_form: function(options, ajax_options) {
+        if (!options.cant_edit) {
+            edit.submit_prop_add_document_form(options, ajax_options);
+        }
+    },
+
+
+    /**
+     * add a /common/image
+     */
+    prop_add_image: function(prop_section) {
+        alert("Add image not yet implemented");
+        prop_section.removeClass("editing");
+    },
+
+    /**
+     * edit a /common/image
+     */
+    value_edit_image: function(prop_section) {
+        alert("Edit image not yet implemented");
+        prop_section.removeClass("editing");
+    },
 
 
 

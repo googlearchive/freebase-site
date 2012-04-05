5c0d3df139ddd9e9cb662ca4bd7f8723e5c35602
masouras@google.com <masouras@google.com@ff54e9e8-a3ca-d32d-4494-0c755e69c161>
2012-04-05 01:23:24 +0000

    "[sitedeploy] action failed - removing"

--- tags/www/lib/58a/mjt/mjt-template.mf.js
+++
@@ -1,9 +0,0 @@
-[
-  "mjt/header.js",
-  "mjt/util.js",
-  "mjt/markup.js",
-  "mjt/eval.js",
-  "mjt/template.js",
-  "mjt/linker.js",
-  "mjt/mjtjquery.js"
-]
--- tags/www/lib/58a/template/freebase.omf.js
+++
@@ -1,1 +0,0 @@
-["template/json2.js", "template/jquery.factory.js", "template/jquery.localstore.js", "template/jquery.metadata.js", "template/tablesorter.js", "jqueryui/jquery.ui.core.omf.js", "suggest/suggest.omf.js", "template/freebase.js", "template/helpers.js", "permission/permission.js", "incompatible_types/incompatible-types.js", "i18n/i18n.js", "flag/flag.js"]

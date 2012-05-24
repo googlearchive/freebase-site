3d2156dbc6ca687e720f790bc68002fe738c1824
masouras@google.com <masouras@google.com@ff54e9e8-a3ca-d32d-4494-0c755e69c161>
2012-05-24 17:50:40 +0000

    "[sitedeploy] Creating branch data:32"

---
+++ data/32/data.mf.js
@@ -0,0 +1,8 @@
+[
+  "lib/flot/flot.core.mf.js",
+  "lib/jquerytools/tabs.js",
+  "lib/propbox/propbox.mf.js",
+  "lib/template/jquery.infinitescroll.js",
+  "lib/collection/collection.js",
+  "data.js"
+]

f0d339acc07718d66f7c86b9a763ac60ae00e766
masouras@google.com <masouras@google.com@ff54e9e8-a3ca-d32d-4494-0c755e69c161>
2012-06-07 23:25:19 +0000

    "[sitedeploy] Creating branch lib:63"

---
+++ branches/www/lib/63/template/jquery.factory.js
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2012, Google Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following disclaimer
+ * in the documentation and/or other materials provided with the
+ * distribution.
+ *     * Neither the name of Google Inc. nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+/**
+ * A jQuery plugin factory
+ *
+ * 1. var foo_class = $.factory("foo", { proto_methods... });
+ * 2. $("#myelement").foo({options...});
+ * 3. var foo_instance = $("#myelement").data("$.foo");  // returns an instance of $.foo class
+ *
+ * $.factory(name, protoype_dict) will create a $.fn[name] jQuery fn and $[name] class.
+ *
+ * If either of the two already exists, $.factory will throw an error.
+ *
+ * The prototype_dict must contain at least init and _destroy methods.
+ *
+ * The plugin class will have access to this.element and this.options
+ */
+;(function($) {
+  $.factory = function(name, proto) {
+    if ($.fn[name]) {
+      throw "$.fn." + name + " plugin already exists";
+    }
+    else if ($[name]) {
+      throw "$." + name + " class already exists";
+    }
+    $.fn[name] = function(options) {
+      return this.each(function() {
+        var $this = $(this);
+        var inst = $this.data("$."+name);
+        if (inst) {
+          // destroy existing instance
+          inst._destroy();
+        }
+        inst = new $[name]($this, options);
+        $this.data("$."+name, inst);
+      });
+    };
+    $[name] = function(element, options) {
+      this.options = $.extend(true, {}, $[name].defaults, options);
+      this.element = element;
+      this.init();
+    };
+    $.extend($[name].prototype, {
+      init: function() {},
+      _destroy: function() {}
+    }, proto);
+    return $[name];
+  };
+})(jQuery);

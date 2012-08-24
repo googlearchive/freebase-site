54a32df84e38073627dd38c7ea75c52e5d776ac5
jasondouglas@google.com <jasondouglas@google.com@ff54e9e8-a3ca-d32d-4494-0c755e69c161>
2012-08-24 17:48:11 +0000

    "[sitedeploy] Creating branch query:50"

---
+++ branches/www/query/50/editor.mf.js
@@ -0,0 +1,9 @@
+[
+  "lib/jquerytools/tabs.js",
+  "lib/jqueryui/jquery.ui.core.mf.js",
+  "lib/jqueryui/jquery.ui.dialog.js",
+  "lib/template/jquery.nicemenu.js",
+  "lib/template/jquery.radiotoggle.js",
+  "lib/template/chosen.jquery.js",
+  "editor.js"
+]

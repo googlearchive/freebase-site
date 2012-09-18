04ab71dd63730c6abcedf3b394fe5349462e3e7d
masouras@google.com <masouras@google.com@ff54e9e8-a3ca-d32d-4494-0c755e69c161>
2012-09-18 17:13:51 +0000

    "[sitedeploy] action failed - removing"

--- tags/www/lib/83a/propbox/form.omf.js
+++
@@ -1,8 +0,0 @@
-[
-  "jquerytools/js/toolbox.expose.js",
-  "jquerytools/js/overlay.js",
-  "propbox/js/jquery.mqlkey.js",
-  "propbox/js/jquery.validate_input.js",
-  "propbox/js/jquery.data_input.js",
-  "propbox/js/form.js"
-]

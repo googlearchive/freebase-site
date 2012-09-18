d73f3332375354e4f083f65377d4e0aeb7ad6632
masouras@google.com <masouras@google.com@ff54e9e8-a3ca-d32d-4494-0c755e69c161>
2012-09-18 19:33:03 +0000

    "[sitedeploy] Creating tag 56a"

---
+++ tags/www/triples/56a/triples.mf.js
@@ -0,0 +1,7 @@
+[
+  "lib/template/js/jquery.tablesorter.js",
+  "lib/template/js/tablesorter.js",
+  "lib/filter/filters.mf.js",
+  "lib/template/js/jquery.infinitescroll.js",
+  "js/triples.js"
+]

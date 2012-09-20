ff4a0984238b0512472d4bb8eafa29136d8a0f10
masouras@google.com <masouras@google.com@ff54e9e8-a3ca-d32d-4494-0c755e69c161>
2012-09-20 20:37:43 +0000

    "[sitedeploy] Creating branch data:55"

---
+++ data/55/data.mf.js
@@ -0,0 +1,10 @@
+[
+  "lib/template/js/jquery.tablesorter.js",
+  "lib/template/js/tablesorter.js",
+  "lib/flot/flot.core.mf.js",
+  "lib/jquerytools/js/tabs.js",
+  "lib/propbox/propbox.mf.js",
+  "lib/template/js/jquery.infinitescroll.js",
+  "lib/collection/js/collection.js",
+  "js/data.js"
+]

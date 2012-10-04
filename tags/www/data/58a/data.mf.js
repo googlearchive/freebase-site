c9bcb649a4570a5ec5eaef1fc7748262988f821b
masouras@google.com <masouras@google.com@ff54e9e8-a3ca-d32d-4494-0c755e69c161>
2012-10-04 22:15:48 +0000

    "[sitedeploy] Creating tag 58a"

---
+++ tags/www/data/58a/data.mf.js
@@ -0,0 +1,9 @@
+[
+  "lib/template/tablesorter.mf.js",
+  "lib/flot/flot.core.mf.js",
+  "lib/jquerytools/js/tabs.js",
+  "lib/propbox/propbox.mf.js",
+  "lib/template/js/jquery.infinitescroll.js",
+  "lib/collection/js/collection.js",
+  "js/data.js"
+]

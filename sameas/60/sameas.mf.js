9a74ad0f4d90f192bb3fbe9ed9a9cbca35d92c5c
masouras@google.com <masouras@google.com@ff54e9e8-a3ca-d32d-4494-0c755e69c161>
2012-10-09 21:12:21 +0000

    "[sitedeploy] Creating branch sameas:60"

---
+++ sameas/60/sameas.mf.js
@@ -0,0 +1,8 @@
+[
+  "lib/template/tablesorter.mf.js",
+  "lib/filter/filters.mf.js",
+  "lib/propbox/propbox.mf.js",
+  "lib/template/js/jquery.infinitescroll.js",
+  "lib/template/js/jquery.fixedcolumn.js",
+  "js/sameas.js"
+]

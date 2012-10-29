a0387a1c5136b6132d2940f54b5247658bd2c274
jasondouglas@google.com <jasondouglas@google.com@ff54e9e8-a3ca-d32d-4494-0c755e69c161>
2012-10-29 19:57:46 +0000

    "[sitedeploy] Creating branch topic:66"

---
+++ branches/www/topic/66/topic.mf.js
@@ -0,0 +1,8 @@
+[
+  "lib/jquerytools/js/toolbox.expose.js",
+  "lib/jquerytools/js/overlay.js",
+  "lib/filter/filters.mf.js",
+  "lib/propbox/propbox.mf.js",
+  "lib/template/js/jquery.fixedcolumn.js",
+  "js/topic.js"
+]

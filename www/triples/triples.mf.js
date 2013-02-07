3f30312280095734068cdaf9520f4bc638f22a48
daepark@google.com <daepark@google.com@ff54e9e8-a3ca-d32d-4494-0c755e69c161>
2013-02-07 02:03:52 +0000

    Issue 7300049: SITE-1276: user, attribution, provenance writes tabs

--- www/triples/triples.mf.js
+++
@@ -1,7 +0,0 @@
-[
-  "lib/template/tablesorter.mf.js",
-  "lib/filter/filters.mf.js",
-  "lib/template/js/jquery.infinitescroll.js",
-  "lib/template/js/jquery.fixedcolumn.js",
-  "js/triples.js"
-]

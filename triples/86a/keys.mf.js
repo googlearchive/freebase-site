eb23b7534735750a1e283adc17494e0e1dc3a90e
pmikota@google.com <pmikota@google.com@ff54e9e8-a3ca-d32d-4494-0c755e69c161>
2013-02-27 21:41:26 +0000

    "[sitedeploy] Creating tag 86a"

---
+++ triples/86a/keys.mf.js
@@ -0,0 +1,8 @@
+[
+  "lib/propbox/propbox.mf.js",
+  "lib/propbox/form.mf.js",
+  "lib/template/js/jquery.infinitescroll.js",
+  "lib/template/js/jquery.lrulist.js",
+  "js/links_helpers.js",
+  "js/keys.js"
+]

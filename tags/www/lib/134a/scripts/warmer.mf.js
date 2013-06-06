249d731b3ad393581c13bd9864ccf7709bb8d354
pmikota@google.com <pmikota@google.com@ff54e9e8-a3ca-d32d-4494-0c755e69c161>
2013-06-06 17:50:55 +0000

    "[sitedeploy] updated static handlers, cache forever, created static files:  codemirror/codemirror-frame.javascript.mf.css codemirror/codemirror-frame.javascript.mf.js codemirror/codemirror-frame.mf.css codemirror/codemirror-frame.mf.js codemirror/codemirror.mf.js collection/collection-edit.mf.js cuecard/cuecard.mf.css cuecard/cuecard.mf.js discuss/discuss.mf.js editor-bundle.mf.css editor-bundle.mf.js error/error.mf.css flag/flag-edit.mf.js flot/flot.all.mf.js flot/flot.core.mf.js jqueryui/jquery.ui.core.mf.js mjt/mjt-template.mf.js mjt/mjt.mf.js propbox/form.mf.js propbox/propbox-edit.mf.js propbox/propbox.mf.js propbox/suggest_datetime.mf.js schema/schema.mf.js schema/warmer.mf.js scripts/warmer.mf.css scripts/warmer.mf.js suggest/suggest.mf.css suggest/suggest.mf.js template/browse.mf.css template/browse.mf.js template/freebase-inline.mf.js template/freebase.mf.css template/freebase.mf.js template/freebase.ui.mf.css template/modernizr.mf.js template/object.mf.css template/object.mf.js template/tablesorter.mf.js"

--- tags/www/lib/134a/scripts/warmer.mf.js
+++ tags/www/lib/134a/scripts/warmer.mf.js
@@ -1,3 +1,37 @@
-[
-    "scripts/js/warmer.js"
-]
+
+/*
+ * Copyright 2012, Google Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following disclaimer
+ * in the documentation and/or other materials provided with the
+ * distribution.
+ *     * Neither the name of Google Inc. nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Additional Licenses for Third Party components can be found here:
+ * http://wiki.freebase.com/wiki/Freebase_Site_License
+ *
+ */
+(function(c){function g(d){var e=null,e=d?d.next(".warmer"):c(".warmer:first");if(e.length){var a=e,d=a.attr("data-id"),f=c("<span>&nbsp;loading...</span>");c.ajax({url:"/_script_warmer",data:{app:d},dataType:"jsonp",beforeSend:function(){a.addClass("warmer-loading");a.append(f)},success:function(b){var b=b.files||[],d=c("<ul>");c.each(b,function(a,b){var e=c("<li>");e.append("<span>"+b.file+"</span>");d.append(e)});b=d.hide();a.append(b);b.slideDown(function(){f.html("<span>&nbsp;loaded</span>");
+a.addClass("warmer-success")})},error:function(){f.html("<span>&nbsp;error</span>");a.addClass("warmer-error")},complete:function(){a.removeClass("warmer-loading");g(a)}})}}c(function(){g()})})(jQuery,window.freebase);

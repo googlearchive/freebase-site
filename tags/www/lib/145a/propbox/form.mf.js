0fb54d8c80b143931a4792aded5fae0d9b194e7b
pmikota@google.com <pmikota@google.com@ff54e9e8-a3ca-d32d-4494-0c755e69c161>
2013-12-17 00:19:05 +0000

    "[sitedeploy] Creating tag 145a"

---
+++ tags/www/lib/145a/propbox/form.mf.js
@@ -0,0 +1,10 @@
+[
+  "jquerytools/js/toolbox.expose.js",
+  "jquerytools/js/overlay.js",
+  "jqueryui/js/jquery.effects.core.js",
+  "propbox/suggest_datetime.mf.js",
+  "propbox/js/jquery.mqlkey.js",
+  "propbox/js/jquery.validate_input.js",
+  "propbox/js/jquery.data_input.js",
+  "propbox/js/form.js"
+]
